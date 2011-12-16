862361cd802ef41a7ec30267c1d2abff5059fb13
Eric Richardson <e@ericrichardson.com>
2011-12-16 00:47:13 -0800

    Speed up blogs; Add url to UploadedAudio

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,20 +2,31 @@ class BlogsController < ApplicationController
   require 'will_paginate/array'
   layout "blog"
   
+  before_filter :load_blog, :except => [:index]
+  
   def index
     @blogs = Blog.active
     render :layout => "application"
   end
   
   def show
-    @blog = Blog.where(:slug => params[:blog]).first
-    @entries = @blog.entries.published.all.reverse.paginate(:page => params[:page], :per_page => 5)
+    @entries = @blog.entries.published.paginate(
+      :page => params[:page],
+      :per_page => 5
+    )
+
     render :layout => "bloglanding"
   end
   
   def entry
-    @blog = Blog.where(:slug => params[:blog]).first
     @entry = @blog.entries.published.find(params[:id])
-    @entries = @blog.entries.published.all.last(5).reverse
+    @entries = @blog.entries.published.last(5).reverse
+  end
+  
+  protected
+  def load_blog
+    @blog = Blog.find_by_slug(params[:blog])
+  rescue
+    redirect_to blogs_path()
   end
 end
--- app/models/uploaded_audio.rb
+++ app/models/uploaded_audio.rb
@@ -2,4 +2,8 @@ class UploadedAudio < ActiveRecord::Base
   set_table_name 'rails_media_uploadedaudio'
   
   belongs_to :content, :polymorphic => true
+  
+  def url
+    "http://media.scpr.org/#{self.mp3_file}"
+  end
 end
