af29a20ba5e1bcd9cf797ca6dbf0e314aed40623
Eric Richardson <erichardson@kpcc.org>
2011-12-16 11:49:35 -0800

    Take status and publish date out of contentcategory view

--- db/migrate/20111207193506_add_view_for_content_category.rb
+++ db/migrate/20111207193506_add_view_for_content_category.rb
@@ -6,33 +6,11 @@ class AddViewForContentCategory < ActiveRecord::Migration
       	c.id,
         c.category_id,
       	c.object_id as content_id,
-        m.class_name as content_type,
-      	COALESCE(news_story.status,shows_segment.status,blogs_entry.status,contentbase_contentshell.status) as status,
-      	COALESCE(news_story.published_at,shows_segment.created_at,blogs_entry.published_at,contentbase_contentshell.pub_at) as pub_date
+        m.class_name as content_type
       from 
       	contentbase_contentcategory as c
       left join
           rails_content_map as m on m.id = c.content_type_id
-      left join
-      	news_story 
-      on
-      	news_story.id = c.object_id
-      	and c.content_type_id = 15
-      left join
-      	shows_segment
-      on 
-      	shows_segment.id = c.object_id
-      	and c.content_type_id = 24
-      left join
-      	blogs_entry
-      on
-      	blogs_entry.id = c.object_id
-      	and c.content_type_id = 44
-      left join
-        contentbase_contentshell
-      on
-        contentbase_contentshell.id = c.object_id
-        and c.content_type_id = 115
     ")
     
   end
