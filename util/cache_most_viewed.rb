61f48dd2bfc779a98cd1bdbd30cc402a706decfa
Eric Richardson <e@ericrichardson.com>
2011-12-19 13:25:38 -0800

    Working cache script for most popular content (page views)

--- util/cache_most_viewed.rb
+++ util/cache_most_viewed.rb
@@ -8,43 +8,35 @@ end
 
 # this is an interim process to work with the current mercer tracker
 
-#r = Redis.new(:host => "localhost", :port => 6380, :db => 1)
+r = Redis.new(:host => "10.226.4.234", :port => 6379, :db => 1)
 $stderr.puts "connected."
 
 views = {}
 
-if false
-  r.smembers('tag_dates').each do |tag|
-    # split for obj_key::date
-    (key,date) = tag.split("::")
-    #$stderr.puts "tag is #{tag}"
-  
-    # parse date to see if we care
-    date = Date.parse(date)
-  
-    # ignore tags more than two days old
-    if Date.today - date > 2
-      next
-    end
-    
-    # init record
-    if !views[tag]
-      views[tag] = 0
-    end
+r.smembers('tag_dates').each do |tag|
+  # split for obj_key::date
+  (key,date) = tag.split("::")
+  #$stderr.puts "tag is #{tag}"
 
-    # get a count for tag date
-    views[tag] += r.hget(tag,'count').to_i
-  
-    #$stderr.puts "counting #{tag}\t#{views[tag]}"
-  
+  # parse date to see if we care
+  date = Date.parse(date)
+
+  # ignore tags more than two days old
+  if Date.today - date > 2
+    next
   end
-end
+  
+  # init record
+  if !views[key]
+    views[key] = 0
+  end
 
-views["news/story:30389"] = 7325
-views["news/story:9070"] = 1526
-views["shows/segment:21785"] = 705
-views["blogs/entry:4021"] = 600
-views["news/story:30396"] = 513
+  # get a count for tag date
+  views[key] += r.hget(tag,'count').to_i
+
+  #$stderr.puts "counting #{tag}\t#{views[tag]}"
+
+end
 
 $stderr.puts "in sort phase"
 
@@ -52,9 +44,7 @@ $stderr.puts "in sort phase"
 
 content = []
 
-views.sort_by { |k,v| v }.reverse()[0..9].each do |k,v|
-  puts "#{k}\t#{v}"
-  
+views.sort_by { |k,v| v }.reverse()[0..9].each do |k,v|  
   obj = ContentBase.obj_by_key(k)
   
   if obj
@@ -62,5 +52,5 @@ views.sort_by { |k,v| v }.reverse()[0..9].each do |k,v|
   end
 end
 
-top_traffic = view.render(:partial => "shared/widgets/most_popular_viewed", :content => content)
+top_traffic = view.render(:partial => "shared/widgets/most_popular_viewed", :object => content, :as => :content)
 Rails.cache.write("widget/popular_viewed",top_traffic)
