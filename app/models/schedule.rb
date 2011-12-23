3a6e86cc66bd102e5b02e0cd7202835d61393648
Eric Richardson <e@ericrichardson.com>
2011-12-23 14:59:57 -0800

    Add "on now" / "up next" logic to the Schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,9 +3,31 @@ class Schedule < ActiveRecord::Base
 
   belongs_to :kpcc_program, :class_name => "KpccProgram", :polymorphic => true
   belongs_to :other_program, :class_name => "OtherProgram", :polymorphic => true
+
+  def self.on_now
+    # find program matching today's week day and current time
+    program = self.where("day = ? and start_time < ?",Date.today.wday,Time.now.to_s(:time)).order("start_time desc").first
+    
+    return program
+  end
+  
+  #----------
   
   def programme
     self.kpcc_program || self.other_program
+  end
+  
+  #----------
+  
+  def up_next
+    # if our current program ends before the end of the day, we want whatever comes on next today
+    if self.end_time.hour > 0
+      # today...
+      return self.class.where("day = ? and start_time >= ?",self.day,self.end_time.to_s(:time)).order("start_time asc").first
+    else
+      # ends at midnight, so we want the first show tomorrow
+      return self.class.where("day = ?", ( self.day <= 5 ? self.day + 1 : 0 ) ).order("start_time asc").first
+    end
   end
   
 end
