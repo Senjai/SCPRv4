161bfe7d114141bc70e0cbcea1e65584befc919d
Eric Richardson <erichardson@kpcc.org>
2011-12-30 10:19:07 -0800

    Wire up latest blog entries for news and arts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
   
   def set_up_finders
     @g_upcoming_events_forum = Event.published.upcoming_forum.limit(4)
+    
+    # FIXME: Isn't there a way to do this without hardcoding the table name in the where clause?
+    @g_latest_blogs_news = BlogEntry.published.joins(:blog).where("blogs_blog.is_news = true").order("published_at desc")
+    @g_latest_blogs_arts = BlogEntry.published.joins(:blog).where("blogs_blog.is_news = false").order("published_at desc")
   end
   
   def check_session
