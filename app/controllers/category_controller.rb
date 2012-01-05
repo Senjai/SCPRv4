8a326edf755870c61de4de97ef3e48d7f98c1ebd
Eric Richardson <erichardson@kpcc.org>
2012-01-04 16:27:39 -0800

    Tweaks to section pages

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,7 +7,7 @@ class CategoryController < ApplicationController
     @content = ThinkingSphinx.search '',
       :classes    => [NewsStory,BlogEntry,ContentShell,ShowSegment],
       :page       => params[:page],
-      :per_page   => 15,
+      :per_page   => 10,
       :order      => :published_at,
       :sort_mode  => :desc,
       :with       => { :category => @category.id }   
@@ -23,7 +23,7 @@ class CategoryController < ApplicationController
     @content = ThinkingSphinx.search '',
       :classes    => [NewsStory,BlogEntry,ContentShell,ShowSegment],
       :page       => params[:page],
-      :per_page   => 15,
+      :per_page   => 10,
       :order      => :published_at,
       :sort_mode  => :desc,
       :with       => { :category => @category.id }    
