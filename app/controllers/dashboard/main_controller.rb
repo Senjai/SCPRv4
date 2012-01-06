ead96aee3e5fe82f6af93910b641ce15b873eba1
Eric Richardson <erichardson@kpcc.org>
2012-01-06 09:59:53 -0800

    Add story counts to dashboard display

--- app/controllers/dashboard/main_controller.rb
+++ app/controllers/dashboard/main_controller.rb
@@ -1,16 +1,18 @@
 class Dashboard::MainController < ApplicationController
   
   def index
+    # get a baseline published stories count
+    @baseline = NewsStory.published.this_week
+    
     # fetch news stories from the last seven days with no category
-    @stories = NewsStory.published.where(
-      "published_at > ?", Date.today - 7
-    ).joins(
+    @stories = @baseline.joins(
       "left join rails_contentbase_contentcategory on `rails_contentbase_contentcategory`.`content_id` = `news_story`.`id` AND `rails_contentbase_contentcategory`.`content_type` = 'NewsStory'"
     ).where(
       "`rails_contentbase_contentcategory`.id is null"
     ).order("published_at desc")
     
-    @no_assets = NewsStory.published.this_week.joins(
+    # and news stories published in the last seven days with no assets
+    @no_assets = @baseline.joins(
       "left join `rails_assethost_contentasset` on `rails_assethost_contentasset`.content_id = news_story.id and `rails_assethost_contentasset`.content_type = 'NewsStory'"
     ).where("rails_assethost_contentasset.id is null").order("published_at desc")
   end
