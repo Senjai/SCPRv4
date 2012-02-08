c283ee2bfa7457fc9d65235e8532d0971b3fe046
Eric Richardson <erichardson@kpcc.org>
2012-02-08 14:40:32 -0800

    Check in some initial ContentBase tests
    * NOTE: You need to apply grants for views to work and tests to run:
    
        grant select on mercer_test.* to mercer@10.226.4.238;
        grant select on mercer_test.* to mercer@10.226.4.227;
        flush privileges;

---
+++ spec/models/content_base_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe ContentBase do
+  ContentBase.content_classes.each do |c|
+    describe c do
+      it "can load an object" do
+        obj = c.order("RAND()").first
+        obj.is_a?(ContentBase).should == true
+      end
+      
+      it "can limit by published content" do 
+        obj = c.published.first
+        obj.is_a?(ContentBase).should == true
+      end
+      
+      it "must return a headline" do
+        obj = c.published.order("RAND()").first
+        obj.headline.should_not == false        
+      end
+      
+      it "must return a short_headline" do
+        obj = c.published.order("RAND()").first
+        obj.short_headline.should_not == false        
+      end
+      
+      it "must return a teaser" do
+        obj = c.published.order("RAND()").first
+        obj.teaser.should_not == false        
+      end
+      
+      it "must have a byline_elements attribute/method" do
+        c.attribute_method?(:byline_elements).should == true        
+      end
+      
+      it "must return byline_elements as an array" do
+        obj = c.published.order("RAND()").first
+        obj.byline_elements.is_a?(Array).should == true        
+      end
+      
+    end
+  end
+end
