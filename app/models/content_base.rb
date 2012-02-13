63c22dc2af69e700e640deb5ca374f1149a08490
Eric Richardson <erichardson@kpcc.org>
2012-02-13 13:57:42 -0800

    Add explicit ordering on assets

--- app/models/content_base.rb
+++ app/models/content_base.rb
@@ -38,7 +38,7 @@ class ContentBase < ActiveRecord::Base
   }
 
   # All ContentBase objects have assets and alarms
-  has_many :assets, :class_name => "ContentAsset", :as => :content
+  has_many :assets, :class_name => "ContentAsset", :as => :content, :order => "asset_order asc"
   has_many :bylines, :class_name => "ContentByline", :as => :content
   
   has_many :brels, :class_name => "Related", :as => :content
