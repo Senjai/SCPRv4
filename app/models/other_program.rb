c7f43cbd1b2af67d42c78d21233d004ecaf27fec
Sean Dillingham <sean.dillingham@gmail.com>
2012-02-24 11:58:01 -0800

    Finished initial implementation of Broadcast Bar Episode Guide modal

--- app/models/other_program.rb
+++ app/models/other_program.rb
@@ -2,4 +2,8 @@ class OtherProgram < ActiveRecord::Base
   self.table_name =  'programs_otherprogram'  
   
   has_many :schedules, :foreign_key => "other_program_id", :class_name => "Schedule"
+  
+  def is_segmented
+    false
+  end
 end
