dc8b2abf3ea5bdfd0bdeaca022e6afe40204e668
Sean Dillingham <sean.dillingham@gmail.com>
2012-02-28 16:40:09 -0800

    Fixed issues identified in pull request

--- app/assets/javascripts/zbase.js
+++ app/assets/javascripts/zbase.js
@@ -14,6 +14,19 @@ jQuery(document).ready(function() {
 	// Taggle Episode Modal Visibility
 	$(".broadcastbar .true, .modal-close").click(function() {
 		$(".episode-guide").toggleClass("hidden");
+		$("body").bind(
+	       'click',
+	       function(e){
+	        if(
+	         !jQuery(".episode-guide").hasClass("hidden")
+			 && !jQuery(e.target).is(".episode-guide")
+			 && !jQuery(e.target).closest(".episode-guide").length
+	        ){
+	         jQuery(".episode-guide").addClass("hidden");
+			 return false;
+	        }
+	       }
+	      );
 		return false;
 	});
 	
