a6672c2ca31f2d6b364f44becac742407313a840
Bryan Ricker <bricker88@gmail.com>
2012-03-06 13:50:59 -0800

    Removing old controller specs

--- spec/controllers/episodes_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe EpisodesController do
-  it "is alive" do
-    true # Pending
-  end
-end
--- spec/controllers/segments_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe SegmentsController do
-  it "is alive" do
-    true
-  end
-end
