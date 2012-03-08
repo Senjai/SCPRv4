7e3ecf9dda64622fbf9a0af22ef6e4c5a0854f78
Eric Richardson <e@ericrichardson.com>
2012-03-08 08:13:33 -0800

    Implement caching on podcasts and feeds
    * Upgrade to Rails 3.2.2 to meet new redis-activesupport requirements
    * Content updates will expire caches and trigger regeneration

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,6 +2,11 @@ class FeedsController < ApplicationController
   layout nil
   
   def all_news
+    # check if we have a cached podcast.  If so, short-circuit and return it
+    if cache = Rails.cache.fetch("feeds:all_news")
+      render :text => cache, :formats => [:xml] and return
+    end
+    
     @feed = {
       :title => "All News || 89.3 KPCC",
       :description => "All news from KPCC's reporters, bloggers and shows."
@@ -19,6 +24,8 @@ class FeedsController < ApplicationController
       :without    => { :category => '' }
     )
     
-    render :action => "feed.xml"
+    xml = render_to_string :action => "feed", :formats => [:xml]    
+    Rails.cache.write_entry("feeds:all_news",xml,:objects => [@content,"contentbase:new"].flatten)
+    render :text => xml, :formats => [:xml]    
   end
 end
--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -9,19 +9,29 @@ class PodcastsController < ApplicationController
   def podcast
     # is this valid?
     @podcast = Podcast.where(:slug => params[:slug]).first
+    @obj_type = nil
     
     if !@podcast
       # not valid... 
       render :text => "Invalid podcast slug.", :status => :not_found and return
     end
+
+    # check if we have a cached podcast.  If so, short-circuit and return it
+    if cache = Rails.cache.fetch("podcast:#{@podcast.id}")
+      render :text => cache, :formats => [:xml] and return
+    end
     
     @content = nil
     if @podcast.item_type == "episodes"
       @content = ( @podcast.program ? @podcast.program.episodes : ShowEpisode ).published
+      @obj_type = "shows/episode:new"
     elsif @podcast.item_type == "segments"
       @content = ( @podcast.program ? @podcast.program.segments : ShowSegment ).published
+      @obj_type = "shows/segment:new"
     elsif @podcast.item_type == "content"
       # TBD
+      @obj_type = "contentbase:new"
+      
     else
       # nothing...
     end
@@ -31,9 +41,14 @@ class PodcastsController < ApplicationController
       @content = @content.first(25).collect { |c| c.audio.any? ? c : nil }.compact
     end
     
-    Rails.logger.debug "content is #{@content}"
+    # we limit podcasts to 15 items
+    @content = @content.first(15)
+        
+    xml = render_to_string :formats => [:xml]
     
-    render "podcast.xml"
+    Rails.cache.write_entry("podcast:#{@podcast.id}",xml,:objects => [@content,@obj_type].flatten)
+    
+    render :text => xml, :formats => [:xml]
   end
   
   #----------
