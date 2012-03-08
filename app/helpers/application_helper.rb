3b65e1d9fd0c910b8fc457c07fe9bfe0ce8c8a0b
Bryan Ricker <bricker88@gmail.com>
2012-03-08 12:29:06 -0800

    Split ApplicationHelper into WidgetsHelper
    Just for a little organization
    
    TODO:
    - Need to test some of the helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-module ApplicationHelper  
+module ApplicationHelper
   
   # render_content takes a ContentBase object and a context, and renders 
   # using the most specific version of that context it can find.
@@ -197,41 +197,6 @@ module ApplicationHelper
     end
   end
   
-  #----------
-  
-  def featured_comment(opts)
-    opts = { :style => "default", :bucket => nil, :comment => nil }.merge(opts||{})
-        
-    comment = nil
-    
-    if opts[:comment]
-      if opts[:comment].is_a?(FeaturedComment)
-        comment = opts[:comment]
-      else
-        begin
-          comment = FeaturedComment.find(opts[:comment])
-        rescue
-          return ''
-        end
-      end
-    elsif opts[:bucket]
-      bucket = FeaturedCommentBucket.find(opts[:bucket])
-      comment = bucket.comments.published.first()
-    else
-      comment = FeaturedComment.published.first()
-    end
-    
-    if comment && comment.content
-      #begin
-        return render(:partial => "shared/featured_comment/#{opts[:style]}", :object => comment, :as => :comment)
-      #rescue
-      #  return render(:partial => "shared/featured_comment/default", :object => comment, :as => :comment)          
-      #end
-    end
-  end
-  
-  #----------
-  
   # Convert a given number of seconds into a human-readable duration. 
   
   def format_duration(secs)
@@ -306,14 +271,4 @@ module ApplicationHelper
     options[:title] ||= object.headline
     content_tag :div, link_to(title, object.audio.first.url, options), class: "story-audio inline"
   end
-  
-  def comment_count_for(object, options={})
-    options[:class] = "comment-count #{options[:class]}"
-    link_to "Comments (#{object.comment_count})", object.link_path(anchor: "comments"), options
-  end
-  
-  def article_meta_for(object)
-    render 'shared/cwidgets/article_meta', content: object
-  end
-    
 end
