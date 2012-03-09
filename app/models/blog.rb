32bc4d2bc204e4c957f7b8300ce3b673b975c0e0
Bryan Ricker <bricker88@gmail.com>
2012-03-09 08:35:35 -0800

    Minor bugfix in blog.rb

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ActiveRecord::Base
   self.table_name =  'blogs_blog'
   
   has_many :entries, :order => 'published_at desc', class_name: "BlogEntry"
-  has_many :tags, :through => :blog_entries
+  has_many :tags, :through => :entries
   
   scope :active, where(:is_active => true)
   scope :is_news, where(:is_news => true)
