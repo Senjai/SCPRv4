4c53d4beacb8267490b7195342254df98719f3f9
Eric Richardson <erichardson@kpcc.org>
2012-03-16 12:31:09 -0700

    Implement Audio model, using consolidated media.Audio from mercer

---
+++ app/models/audio.rb
@@ -0,0 +1,15 @@
+class Audio < ActiveRecord::Base
+  self.table_name =  'rails_media_audio'
+  self.primary_key = :id
+  
+  belongs_to :content, :polymorphic => true
+  
+  def url
+    "http://media.scpr.org/#{self.mp3_file}"
+  end
+  
+  def mp3_path
+    Rails.application.config.scpr.media_root ? [Rails.application.config.scpr.media_root,self.mp3_file].join('/') : false
+  end
+  
+end
