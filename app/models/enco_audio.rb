60d43987366e3f309e87ba5a40d16953a40f4e00
Eric Richardson <erichardson@kpcc.org>
2012-03-16 12:46:22 -0700

    Remove models for old audio

--- app/models/enco_audio.rb
+++
@@ -1,8 +0,0 @@
-class EncoAudio < ActiveRecord::Base
-  self.table_name =  'media_encoaudio'
-  
-  def mp3_path
-    Rails.application.config.scpr.media_root ? self.url.sub("http://media.scpr.org",Rails.application.config.scpr.media_root) : false
-  end
-  
-end
