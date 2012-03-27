678abc67be99206adacda593f455574e4ad4b8f1
Eric Richardson <erichardson@kpcc.org>
2012-03-27 14:44:57 -0700

    Fix default audio scope to exclude both null and empty string

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -4,7 +4,8 @@ class Audio < ActiveRecord::Base
   
   belongs_to :content, :polymorphic => true
 
-  default_scope where("mp3 is not null")
+  default_scope where("mp3 is not null and mp3 != ''")
+  scope :with_enco, where("enco_number is not null and enco_date is not null")
   
   def url
     "http://media.scpr.org/#{self.mp3}"
