536af8a1f4eb4f74d400fd11e76319362d9b3764
Eric Richardson <erichardson@kpcc.org>
2012-03-27 15:26:15 -0700

    Add ENCO dashboard for finding audio that needs uploading

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -5,7 +5,8 @@ class Audio < ActiveRecord::Base
   belongs_to :content, :polymorphic => true
 
   default_scope where("mp3 is not null and mp3 != ''")
-  scope :with_enco, where("enco_number is not null and enco_date is not null")
+  scope :with_enco, where("enco_number is not null and enco_number != 0 and enco_date is not null")
+  scope :awaiting_enco, with_enco.where("mp3 is null or mp3 = ''")
   
   def url
     "http://media.scpr.org/#{self.mp3}"
