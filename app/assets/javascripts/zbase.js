7a631501dda0edf72f1233f9cc3ccba25df6350b
Bryan Ricker <bricker88@gmail.com>
2012-03-27 18:27:20 -0700

    Remove old z-base code for modal popup

--- app/assets/javascripts/zbase.js
+++ app/assets/javascripts/zbase.js
@@ -1,25 +1,3 @@
-jQuery(document).ready(function() {	
-	// Taggle Episode Modal Visibility
-   // $(".broadcastbar .true, .modal-close").click(function() {
-   // 	$(".episode-guide").toggleClass("hidden");
-   // 	$("body").bind(
-   //        'click',
-   //        function(e){
-   //         if(
-   //          !jQuery(".episode-guide").hasClass("hidden")
-   // 		 && !jQuery(e.target).is(".episode-guide")
-   // 		 && !jQuery(e.target).closest(".episode-guide").length
-   //         ){
-   //          jQuery(".episode-guide").addClass("hidden");
-   // 		 return false;
-   //         }
-   //        }
-   //       );
-   // 	return false;
-   // });
-	
-});
-
 // Open Listen Live Window
 function open_popup(url) { 
         window.open(url, 'pop_up','height=800,width=556,resizable,left=10,top=10,scrollbars=no,toolbar=no');
