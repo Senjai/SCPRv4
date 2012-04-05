ccdfd437fc44e542158def3cd2a1881bf127fb63
Bryan Ricker <bricker88@gmail.com>
2012-04-04 17:00:44 -0700

    WIP Responsive Design - Images work
    - Lots of work with attempting to make images fluid
    - Started visiting other pages in Beta and working with them to at
    least look decent again

--- app/assets/javascripts/zbase.js
+++ app/assets/javascripts/zbase.js
@@ -16,13 +16,16 @@ function animateMenu(percentage) {
 // temporary
 $(document).ready(function() {
 	
-	$(".nav-toggler").toggle(function() {
+	var $toggler = $(".nav-toggler");
+	var $compactNav = $(".nav-compact");
+	
+	$toggler.toggle(function() {
 		animateMenu("0%");
 	}, function() {
 		animateMenu("-50%");
 	});
 	
-	$(".nav-toggler").touchwipe({
+	$toggler.touchwipe({
 	     wipeLeft: function() { animateMenu("-50%"); },
 	     wipeRight: function() { animateMenu("0%"); },
 	     min_move_x: 20,
@@ -30,6 +33,12 @@ $(document).ready(function() {
 	     preventDefaultEvents: true
 	});
 	
+	$("body").on("click", function(event) {
+    	if($compactNav.is(":visible") && !$(event.target).is($toggler) && !$(event.target).is($compactNav) && !$(event.target).closest($compactNav).length) {
+        	// FIXME Doesn't technically "Toggle" the menu, so you have to click it twice to open it again. 
+			animateMenu("-50%");
+		}
+	});
 });
 
 // Check for mobile
