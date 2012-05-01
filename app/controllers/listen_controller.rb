514e7a151ca9dc08307e2a29b2f7e78396025232
Eric Richardson <erichardson@kpcc.org>
2012-04-30 17:16:51 -0700

    Wire up new Listen Live player
    * ListenLive: Create CurrentGen class for player functionality
    * Add link_path function to KPCCProgram and OtherProgram
    * Return link in Schedule JSON

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -1,6 +1,12 @@
 class ListenController < ApplicationController
   
   def index
+    # grab eight hours worth of schedule, starting now
+    @schedule = Schedule.at :time => Time.now(), :hours => 8
+    
+    # grab our homepage stories
+    @homepage = Homepage.published.first
+    
     render :layout => false
   end
   
