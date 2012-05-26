ee55c884a48f5f38e171211bdb8834bf8ceb946f
Sean Dillingham <sean.dillingham@gmail.com>
2012-05-25 17:50:32 -0700

    styled breaking news alerts and added a model method to return user-friendly alert types

--- app/models/breaking_news_alert.rb
+++ app/models/breaking_news_alert.rb
@@ -1,6 +1,16 @@
 class BreakingNewsAlert < ActiveRecord::Base
   self.table_name = 'layout_breakingnewsalert'
   
+  def break_type
+    if alert_type = "break"
+      "Breaking News"
+    elsif alert_type = "audio"
+      "Listen Live"
+    elsif alert_type = "now"
+      "Happening Now"
+    end  
+  end
+  
   def self.get_alert
     alert = self.order("created_at desc").first
     if alert.present? and alert.is_published
