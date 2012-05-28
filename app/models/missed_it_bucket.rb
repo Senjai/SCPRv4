fcaf371b0d2e3abe4c75ad97e6afcb06407b3cb8
Bryan Ricker <bricker88@gmail.com>
2012-05-27 18:28:24 -0700

    Implement caching on missed it buckets

--- app/models/missed_it_bucket.rb
+++ app/models/missed_it_bucket.rb
@@ -1,5 +1,8 @@
 class MissedItBucket < ActiveRecord::Base
   self.table_name = "contentbase_misseditbucket"
-  
   has_many :contents, class_name: "MissedItContent", foreign_key: "bucket_id", order: "position asc"
+  
+  def obj_key
+    "missed_it:#{id}"
+  end
 end
