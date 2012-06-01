6a5b81d4e9c54de851f3340f5fea9642704181a6
Bryan Ricker <bricker88@gmail.com>
2012-06-01 13:03:58 -0700

    Pull in session store from railsadmin with YAML fixes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,18 +1,23 @@
 # Be sure to restart your server when you modify this file.
 
-class JSONVerifier < ActiveSupport::MessageVerifier
+class YAMLVerifier < ActiveSupport::MessageVerifier
   def verify(signed_message)
     raise InvalidSignature if signed_message.blank?
 
     data, digest = signed_message.split("--")
-    
-    if data.present? && digest.present? && secure_compare(digest, generate_digest(data))      
-      ActiveSupport::JSON.decode(Base64.decode64(data.gsub('%3D','=')))
+    if data.present? && digest.present? && secure_compare(digest, generate_digest(data))
+      # First load with @serializer (YAML), if there is a YAML syntax error, then decode with JSON
+      begin
+        @serializer.load(::Base64.decode64(data))
+      rescue Psych::SyntaxError
+        Rails.logger.debug "Caught YAML syntax error. Decoding with JSON."
+        ActiveSupport::JSON.decode(Base64.decode64(data.gsub('%3D','=')))    
+      end
     else
       raise InvalidSignature
     end
   end
-
+  
   def generate(value)
     # If it isn't present, add in session_expiry to support django
     if value.is_a?(Hash) && !value.has_key?("_session_expiry")
@@ -20,7 +25,7 @@ class JSONVerifier < ActiveSupport::MessageVerifier
       value['_session_expiry'] = (Time.now() + 30*86400).strftime("%s")
     end
     
-    data = Base64.strict_encode64(ActiveSupport::JSON.encode(value))
+    data = ::Base64.strict_encode64(@serializer.dump(value))
     "#{data}--#{generate_digest(data)}"
   end
 end
@@ -31,7 +36,7 @@ module ActionDispatch
       def initialize(parent_jar, secret)
         ensure_secret_secure(secret)
         @parent_jar = parent_jar
-        @verifier   = JSONVerifier.new(secret)
+        @verifier   = YAMLVerifier.new(secret, serializer: YAML)
       end
     end
   end
