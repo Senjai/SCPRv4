7976df7328071f9d5eadd02e878ed9ff456d59df
Bryan Ricker <bricker88@gmail.com>
2012-06-01 17:40:11 -0700

    Combined file

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,79 @@
+# Be sure to restart your server when you modify this file.
+
+class YAMLVerifier < ActiveSupport::MessageVerifier
+  def verify(signed_message)
+    raise InvalidSignature if signed_message.blank?
+
+    data, digest = signed_message.split("--")
+    if data.present? && digest.present? && secure_compare(digest, generate_digest(data))
+      # First load with @serializer (YAML), if there is a YAML syntax error, then decode with JSON
+      begin
+        @serializer.load(::Base64.decode64(data))
+      rescue Psych::SyntaxError
+        Rails.logger.debug "Caught YAML syntax error. Decoding with JSON."
+        ActiveSupport::JSON.decode(Base64.decode64(data.gsub('%3D','=')))    
+      end
+    else
+      raise InvalidSignature
+    end
+  end
+  
+  def generate(value)
+    # If it isn't present, add in session_expiry to support django
+    if value.is_a?(Hash) && !value.has_key?("_session_expiry")
+      # expire in 30 days
+      value['_session_expiry'] = (Time.now() + 30*86400).strftime("%s")
+    end
+    
+    data = ::Base64.strict_encode64(@serializer.dump(value))
+    "#{data}--#{generate_digest(data)}"
+  end
+end
+
+
+class JSONVerifier < ActiveSupport::MessageVerifier
+  def verify(signed_message)
+    raise InvalidSignature if signed_message.blank?
+
+    data, digest = signed_message.split("--")
+    
+    if data.present? && digest.present? && secure_compare(digest, generate_digest(data))      
+      ActiveSupport::JSON.decode(Base64.decode64(data.gsub('%3D','=')))
+    else
+      raise InvalidSignature
+    end
+  end
+
+  def generate(value)
+    # If it isn't present, add in session_expiry to support django
+    if value.is_a?(Hash) && !value.has_key?("_session_expiry")
+      # expire in 30 days
+      value['_session_expiry'] = (Time.now() + 30*86400).strftime("%s")
+    end
+    
+    data = Base64.strict_encode64(ActiveSupport::JSON.encode(value))
+    "#{data}--#{generate_digest(data)}"
+  end
+end
+
+
+module ActionDispatch
+  class Cookies
+    class SignedCookieJar
+      def initialize(parent_jar, secret)
+        ensure_secret_secure(secret)
+        @parent_jar = parent_jar
+        @verifier   = YAMLVerifier.new(secret, serializer: YAML)
+        #@verifier   = JSONVerifier.new(secret)
+      end
+    end
+  end
+end
+      
+
+Scprv4::Application.config.session_store :cookie_store, key: '_scprv4_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scprv4::Application.config.session_store :active_record_store
