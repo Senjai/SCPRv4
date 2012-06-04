8d469d990d182ea2c55dc55cc3843a23d7c31e46
Bryan Ricker <bricker88@gmail.com>
2012-06-04 13:35:20 -0700

    Add /breaking_email path to test email

--- app/controllers/breaking_news_controller.rb
+++ app/controllers/breaking_news_controller.rb
@@ -1,5 +1,9 @@
 class BreakingNewsController < ApplicationController
-  def breaking_news
-    @breaking_news_alert = BreakingNewsAlert.get_alert
+  layout false
+  
+  def show
+    if @breaking_news_alert = BreakingNewsAlert.get_alert
+      render(template: "/breaking_news/email/template", locals: { alert: @breaking_news_alert }) and return
+    end
   end
 end
