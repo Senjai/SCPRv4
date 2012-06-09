3bd3b8b376f6f5653d981953a31e975b6f22d87e
Bryan Ricker <bricker88@gmail.com>
2012-06-08 19:23:43 -0700

    Refactor content_base_helpers a bit

--- spec/support/content_base_helpers.rb
+++ spec/support/content_base_helpers.rb
@@ -1,13 +1,26 @@
 module ContentBaseHelpers
   def make_content(num=5) # Creates `num` of each ContentBase subclass for helping with Sphinx tests
+    records = []
     ContentBase.content_classes.each do |klass|
-      FactoryGirl.create_list(klass.to_s.underscore.to_sym, num.to_i, with_category: true)
+      records += FactoryGirl.create_list(klass.to_s.underscore.to_sym, num.to_i, with_category: true)
     end
+    records
   end
 end
 
 # require './spec/spec_helper.rb'
 # require './spec/support/content_base_helpers.rb'
 # include ContentBaseHelpers
-# ThinkingSphinx::Test.index
-# ThinkingSphinx::Test.start
+
+# -- Example usage for ThinkingSphinx::Test
+
+# before :each do
+#   DatabaseCleaner.strategy = :truncation
+#   ThinkingSphinx::Test.index
+#   ThinkingSphinx::Test.start
+# end
+
+# after :each do
+#   ThinkingSphinx::Test.stop
+#   DatabaseCleaner.strategy = :transaction
+# end
