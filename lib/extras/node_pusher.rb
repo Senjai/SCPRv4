8b18419f31f500dd6f8ddf26c7fdec6572ea9842
Bryan Ricker <bricker88@gmail.com>
2012-06-27 16:04:06 -0700

    movie publisher file and rename

---
+++ lib/extras/node_pusher.rb
@@ -0,0 +1,15 @@
+class NodePusher
+  class << self
+    def publish(action, user, data)
+      url = "#{Rails.application.config.node.server}/notify/#{action}/#{user}"
+      res = Net::HTTP.post_form(URI.parse(URI.encode(url)), data)
+
+      # 200 implies successfully sent.
+      # There is nothing we can do if the targe user is not online(404)
+      # For any other error, raise Exception
+      unless ["200", "404"].include? res.code
+        raise Exception.new("Error: #{res.code}")
+      end
+    end
+  end
+end
