fc37098b46bf730fd8ed4d7e88c381f51539b121
Bryan Ricker <bricker88@gmail.com>
2012-06-29 11:06:16 -0700

    Add asset checking to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,17 +19,23 @@ role :web, "web1.scpr.org", "web2.scpr.org"
 role :db,  "web2.scpr.org", :primary => true
 role :sphinx, "media.scpr.org"
 
+
+# --------------
+# cap staging deploy
 task :staging do
   roles.clear
   set :rails_env, :scprdev
-  set :branch, "designwork"
+  set :branch, "master"
   role :app, "scprdev.org"
   role :web, "scprdev.org"
   role :db,  "scprdev.org", :primary => true
   role :sphinx, "scprdev.org"  
 end
 
+
 namespace :deploy do
+  # --------------
+  # Restart app
   task :start, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
@@ -42,8 +48,24 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
+  
+  # --------------  
+  # Skip asset precompile if nothing was changed
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision) rescue nil
+      
+      if from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
 end
 
+# --------------
+# Sphinx
 namespace :remote_ts do
   task :start, :roles => :sphinx do
     thinking_sphinx.configure
