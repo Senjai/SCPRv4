a74e4715dd4ebf03d3bef575554f5beda9ba382d
Bryan Ricker <bricker88@gmail.com>
2012-07-02 15:02:36 -0700

    Verify connection for Resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,9 @@
 # Use whatever the environment's cache is for Resque
 Resque.redis = Rails.cache.instance_variable_get(:@data)
+
+# Every time a job is started, make sure the connection
+# to MySQL is okay. This avoids the "MySQL server has gone away"
+# error.
+Resque.after_fork = Proc.new { 
+  ActiveRecord::Base.verify_active_connections!  
+}
