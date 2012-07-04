49cc6cbd9c2ec3c4909417482eb8c7805418d415
Bryan Ricker <bricker88@gmail.com>
2012-07-04 02:55:27 -0700

    Add route globbing for pretty URLs

--- config/routes.rb
+++ config/routes.rb
@@ -166,6 +166,7 @@ Scprv4::Application.routes.draw do
   
   # -- RSS feeds -- #
   match '/feeds/all_news' => 'feeds#all_news', :as => :all_news_feed
+  match '/feeds/*feed_path' => redirect { |params| "/#{params[:feed_path]}.xml" }
   
   # -- podcasts -- #
   match '/podcasts/:slug/' => 'podcasts#podcast', :as => :podcast
