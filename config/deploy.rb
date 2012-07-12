95748531c8c09a23c638fd5ba2ee780f9cdc37f7
Bryan Ricker <bricker88@gmail.com>
2012-07-11 18:47:04 -0700

    Updates to deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,9 @@ set :stages, %w{ production staging }
 set :default_stage, "production"
 require 'capistrano/ext/multistage'
 
+
 # --------------
-# Variables for all stages
+# Universal Variables
 
 set :application, "scprv4"
 set :scm, :git
@@ -18,34 +19,58 @@ set :deploy_to, "/web/scprv4"
 set :user, "scprv4"
 set :use_sudo, false
 
+# Pass these in with -s to override: 
+#    cap deploy -s force_assets=true -s force_npm=true
+set :force_assets, false
+set :force_npm, false
+
 # --------------
-# Tasks for all stages
+# Universal Tasks
 
 namespace :deploy do
   # --------------
   # Restart app
-  task :start, :roles => [:app, :workers] do
+  task :start, roles: [:app, :workers] do
     run "touch #{current_release}/tmp/restart.txt"
   end
 
-  task :stop, :roles => [:app, :workers] do
+  task :stop, roles: [:app, :workers] do
     # Do nothing.
   end
 
   desc "Restart Application"
-  task :restart, :roles => [:app, :workers] do
+  task :restart, roles: [:app, :workers] do
     run "touch #{current_release}/tmp/restart.txt"
   end
   
-  # --------------  
+  # --------------
+  # Install & Rebuild packages if package.json was updated
+  # This installs everything into #{latest_release}/node_modules,
+  # moves it to #{shared_path}/node_modules, and then symlinks
+  # #{shared_path}/node_modules into #{latest_release}
+  # This allows the application to share packages between
+  # deploys if possible.
+  task :npm, roles: :web do
+    from = source.next_revision(current_revision) rescue nil
+    if force_npm || from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} package.json | wc -l").to_i > 0
+      run "cd #{latest_release} && npm install && npm rebuild"
+      run "mv -f #{lateset_release}/node_modules/ #{shared_path}"
+    else
+      logger.info "package.json not updated. SKIPPING npm install"
+    end
+    
+    run "ln -nsf #{shared_path}/node_modules/ #{latest_release}"
+  end
+  
+  # --------------
   # Skip asset precompile if no assets were changed
   namespace :assets do
-    task :precompile, :roles => :web do
+    task :precompile, roles: :web do
       from = source.next_revision(current_revision) rescue nil
       
       # Previous revision is blank or git log doesn't 
       # have any new lines mentioning assets
-      if from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      if force_assets || from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
         run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
       else
         logger.info "No changes in assets - SKIPPING asset pre-compilation"
@@ -57,16 +82,16 @@ end
 # --------------
 # Sphinx
 namespace :remote_ts do
-  task :start, :roles => :sphinx do
+  task :start, roles: :sphinx do
     thinking_sphinx.configure
     thinking_sphinx.start
   end
   
-  task :stop, :roles => :sphinx do
+  task :stop, roles: :sphinx do
     thinking_sphinx.stop
   end
   
-  task :index, :roles => :sphinx do 
+  task :index, roles: :sphinx do 
     thinking_sphinx.index
   end
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,35 @@
 set :branch, "master"
 set :rails_env, :scprdev
+set :skip_ts, false
+set :skip_dbcopy, false
 
 role :app,      "scprdev.org"
 role :web,      "scprdev.org"
 role :workers,  "scprdev.org"
 role :db,       "scprdev.org", :primary => true
 role :sphinx,   "scprdev.org"
+#role :dbcopy,   "66.226.4.229"
 
-after "deploy:update_code", "thinking_sphinx:index"
+namespace :thinking_sphinx do
+  namespace :staging do
+    task :index do
+      if skip_ts
+        logger.info "SKIPPING thinking_sphinx:index"
+      else
+        thinking_sphinx.index
+      end
+    end
+  end
+end
+
+namespace :db do
+  namespace :mercer do
+    task :pull, roles: :dbcopy do
+      # todo
+    end
+  end
+end
+
+#after "deploy:update_code", "db:mercer:pull"
+after "deploy:update_code", "thinking_sphinx:staging:index"
+after "deploy:update_code", "deploy:npm"
