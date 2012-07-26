d18520aa1587381fae25f9d840b6a12387d1256c
Bryan Ricker <bricker88@gmail.com>
2012-07-26 16:28:13 -0700

    Remove some unnecessary scripts and pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,15 +5,11 @@
 //= require jquery
 //= require jquery_ujs
 
-//= require handlebars-1.0.0.beta.6.js
-//= require jquery.mousewheel.3.0.4
 //= require jquery.jplayer
 //= require spin.js
 //= require moment
 //= require moment-strftime
-//= require bootstrap-dropdown
 //= require bootstrap-tab
-//= require bootstrap-modal
 //= require content_carousel
 
 //= require audio
