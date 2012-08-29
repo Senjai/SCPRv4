5fae968f9a28798d9be306fa5de62b8f1e56b5c7
Bryan Ricker <bricker88@gmail.com>
2012-08-29 14:15:32 -0700

    Reorder migrations

--- db/migrate/20120829193419_add_homepage_to_rails_content_map.rb
+++
@@ -1,9 +0,0 @@
-class AddHomepageToRailsContentMap < ActiveRecord::Migration
-  def up
-    RailsContentMap.create django_content_type_id: 58, rails_class_name: "Homepage"
-  end
-  
-  def down
-    RailsContentMap.delete_all(rails_class_name: "Homepage")
-  end
-end
--- db/migrate/20120829204443_add_bio_to_rails_content_map.rb
+++
@@ -1,9 +0,0 @@
-class AddBioToRailsContentMap < ActiveRecord::Migration
-  def up
-    RailsContentMap.create django_content_type_id: 13, rails_class_name: "Bio"
-  end
-  
-  def down
-    RailsContentMap.delete_all(rails_class_name: "Bio")
-  end
-end
