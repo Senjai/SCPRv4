f7a305ba8c17bb83817122b7f38d1d8c2580b6c0
Bryan Ricker <bricker88@gmail.com>
2012-10-14 05:56:26 -0700

    Change CarrierWave to move files instead of copy them for faster performance.

--- app/models/audio/program_audio.rb
+++ app/models/audio/program_audio.rb
@@ -12,6 +12,9 @@
 #
 class Audio
   class ProgramAudio < Audio
+    extend LogsAsTask
+    logs_as_task
+    
     before_create :set_description_to_episode_headline, if: -> { self.description.blank? }
   
     def set_description_to_episode_headline
@@ -31,48 +34,57 @@ class Audio
   
       #------------
       # TODO This could be broken up into smaller units
+      # Since this is run as a task, we need some informative
+      # logging in case of failure, hence the begin/rescue block.
       def bulk_sync!
-        # Each KpccProgram with episodes and which can sync audio
-        KpccProgram.can_sync_audio.each do |program|
-          # Each file in this program's audio directory
-          Dir[program.absolute_audio_path].each do |file|
-            absolute_mp3_path = File.join(program.absolute_audio_path, file)
+        begin
+          # Each KpccProgram with episodes and which can sync audio
+          KpccProgram.can_sync_audio.each do |program|
+            # Each file in this program's audio directory
+            Dir[program.absolute_audio_path].each do |file|
+              absolute_mp3_path = File.join(program.absolute_audio_path, file)
         
-            # Move on if:
-            # 1. File already exists (program audio only needs to exist once in the DB)
-            next if existing[File.join(program.audio_dir, file)]
+              # Move on if:
+              # 1. File already exists (program audio only needs to exist once in the DB)
+              next if existing[File.join(program.audio_dir, file)]
         
-            # 2. The filename doesn't match our regex (won't be able to get date)
-            match = file.match(FILENAMES[:program])
-            next if !match
+              # 2. The filename doesn't match our regex (won't be able to get date)
+              match = file.match(FILENAMES[:program])
+              next if !match
             
-            # 3. The file is too old -
-            #    If the file was uploaded more than 14 days ago
-            #    and still hasn't been matched, then something's wrong.
-            #    Maybe the date is incorrect? Either way, at this point
-            #    it's too old to keep trying. They can upload the audio
-            #    manually if they need to.
-            file_date = File.mtime(absolute_mp3_path)
-            next if file_date < 14.days.ago
+              # 3. The file is too old -
+              #    If the file was uploaded more than 14 days ago
+              #    and still hasn't been matched, then something's wrong.
+              #    Maybe the date is incorrect? Either way, at this point
+              #    it's too old to keep trying. They can upload the audio
+              #    manually if they need to.
+              file_date = File.mtime(absolute_mp3_path)
+              next if file_date < 14.days.ago
 
-            # Get the date for this episode/segment based on the filename,
-            # find that episode/segment, and create the audio / association
-            # if the content for that date exists.
-            date = Time.new(match[:year], match[:month], match[:day])
+              # Get the date for this episode/segment based on the filename,
+              # find that episode/segment, and create the audio / association
+              # if the content for that date exists.
+              date = Time.new(match[:year], match[:month], match[:day])
         
-            if program.display_episodes?
-              content = program.episodes.where(air_date: date).first
-            else
-              content = program.segments.where(published_at: date..date.end_of_day).first
-            end
+              if program.display_episodes?
+                content = program.episodes.where(air_date: date).first
+              else
+                content = program.segments.where(published_at: date..date.end_of_day).first
+              end
         
-            if content
-              audio = self.new(content: content, mp3: File.open(absolute_mp3_path))
-              synced << audio if audio.save
-            end
-          end # Dir
-        end # KpccProgram
+              if content
+                audio = self.new(content: content, mp3: File.open(absolute_mp3_path))
+                synced << audio if audio.save!
+                self.log "Saved ProgramAudio ##{audio.id} for #{content.simple_title}"
+              end
+            end # Dir
+          end # KpccProgram
+          
+        rescue Exception => e
+          self.log "Could not save ProgramAudio: #{e}"
+        end
         
+        self.log "Finished syncing ProgramAudio. Total synced: #{synced.size}"
         synced
       end # sync!
     
--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -3,14 +3,31 @@
 class AudioUploader < CarrierWave::Uploader::Base
   storage :file
 
+  #--------------
+  # Override default CarrierWave config
+  # to move files instead of copy them.
+  def move_to_cache
+    true
+  end
+  
+  def move_to_store
+    true
+  end
+
+  #--------------  
+  
   def store_dir
     File.join(Rails.application.config.scpr.media_root, "audio", model.store_dir)
   end
+  
+  #--------------
   
   def raw_value
     model.read_attribute mounted_as
   end
 
+  #--------------
+  # Only allow mp3's
   def extension_white_list
     %w{ mp3 }
   end
