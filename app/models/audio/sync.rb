1339874f22487b6c1bc74305b14edba52ca9afdc
Bryan Ricker <bricker88@gmail.com>
2012-10-14 09:59:33 -0700

    Even better logging

--- app/models/audio/sync.rb
+++ app/models/audio/sync.rb
@@ -17,16 +17,17 @@ class Audio
       def bulk_sync_awaiting_audio!(klass, limit=nil)
         limit ||= 2.weeks.ago
         awaiting = klass.awaiting_audio.where("created_at > ?", limit)
+        synced = 0
         
         if awaiting.empty?
           self.log "No Audio to sync."
         else
         
           awaiting.each do |audio|
-            audio.sync!
+            synced += 1 if audio.sync!
           end
           
-          self.log "Finished. Files synced: #{synced.size}"
+          self.log "Finished. Total synced: #{synced}"
         end
       end
     
@@ -34,18 +35,20 @@ class Audio
       # Enco and Direct audio sync this way
       # Don't use this method directly, use object.sync!
       def sync_if_file_exists!(audio)
-        synced = []
         begin
           if File.exists? audio.full_path
             audio.mp3 = File.open(audio.full_path)
-            synced << audio.save!
+            audio.save!
             self.log "Saved Audio ##{audio.id}: #{audio.full_path}"
+            audio
           else
             self.log "Still awaiting audio file for Audio ##{audio.id}: #{audio.full_path}"
+            false
           end
         rescue Exception => e
           self.log "Could not save Audio ##{audio.id}: #{e}"
-        end        
+          false
+        end
       end
     end # singleton
   end # Sync
