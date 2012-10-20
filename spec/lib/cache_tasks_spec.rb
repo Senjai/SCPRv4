cd769ed731564d3eea0ee4b5430aab86654bbbf2
Bryan Ricker <bricker88@gmail.com>
2012-10-19 17:15:24 -0700

    Add a spec

--- spec/lib/cache_tasks_spec.rb
+++ spec/lib/cache_tasks_spec.rb
@@ -2,6 +2,12 @@ require "spec_helper"
 
 describe CacheTasks::Task do
   describe "#cache" do
-    pending
+    it "Writes the rendered content into cache" do
+      task = CacheTasks::Task.new
+      ActionView::Base.any_instance.should_receive(:render).with(partial: "some/cool/partial", object: "some cool object", as: :content) { "The Cache" }
+      Rails.cache.fetch("some:cool:key").should eq nil
+      task.cache("some cool object", "some/cool/partial", "some:cool:key").should eq true
+      Rails.cache.fetch("some:cool:key").should eq "The Cache"
+    end
   end
 end
