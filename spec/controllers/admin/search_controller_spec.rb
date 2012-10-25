03b420140e7e51038cb531d7f4d32036c69f1fe4
Bryan Ricker <bricker88@gmail.com>
2012-10-25 16:18:31 -0700

    Begin adding Search functionality to admin

--- spec/controllers/admin/search_controller_spec.rb
+++ spec/controllers/admin/search_controller_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 describe Admin::SearchController do
-  pending
+  render_views
+  
+  describe "GET /index" do
+    before :each do
+      controller.stub(:admin_user) { create :admin_user }
+    end
+    
+    it "figures out the class" do
+      get :index, resource: "news_stories"
+      assigns(:klass).should eq NewsStory
+    end
+    
+    it "sends it off to ::search" do
+      NewsStory.should_receive(:search).with("coolsearchbro", kind_of(Hash))
+      get :index, resource: "news_stories", query: "coolsearchbro"
+    end
+  end
 end
