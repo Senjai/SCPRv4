7d7795ec4a0ed7955533de81f67119578bfaa84d
Bryan Ricker <bricker88@gmail.com>
2012-11-02 18:01:33 -0700

    Rescue Admin dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -4,12 +4,7 @@ class Admin::HomeController < Admin::BaseController
     @nav_groups = AdminResource.config.nav_groups
     
     # Gather some data for stats
-    @latest_story = ThinkingSphinx.search('',
-      :classes    => ContentBase.content_classes,
-      :order      => :published_at,
-      :sort_mode  => :desc,
-      :limit      => 1,
-    ).first
+    @latest_story = get_latest_story
     
     @recent_stories  = NewsStory.published.where("published_at >= ?", 1.hour.ago)
     @latest_homepage = Homepage.published.first
@@ -17,5 +12,19 @@ class Admin::HomeController < Admin::BaseController
     # Get the latest activity
     @current_user_activities = @admin_user.activities.order("created_at desc").limit(5)
     @latest_activities       = Secretary::Version.order("created_at desc").where("user_id != ?", @admin_user.id).limit(5)
+  end
+  
+  private
+  def get_latest_story
+    begin
+      ThinkingSphinx.search('',
+        :classes    => ContentBase.content_classes,
+        :order      => :published_at,
+        :sort_mode  => :desc,
+        :limit      => 1,
+      ).first
+    rescue Riddle::ConnectionError
+      nil
+    end
   end
 end
