27f36278ae4487488d986c78249d693a4c8b12bc
Bryan Ricker <bricker88@gmail.com>
2012-11-12 18:45:13 -0800

    remove a file

--- lib/admin_resource/spec/lib/helpers/controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path("../../../spec_helper", __FILE__)
-
-describe AdminResource::Helpers::Controller do
-  let(:controller) { AdminResource::Test::PeopleController.new }
-  let(:person) { Person.create(name: "Bryan") }
-  
-  pending
-  
-end
