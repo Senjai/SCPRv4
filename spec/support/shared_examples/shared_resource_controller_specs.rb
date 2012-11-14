271041e6c87d70c6c4ac7bc1e188e36826d68b54
Bryan Ricker <bricker88@gmail.com>
2012-11-13 17:01:13 -0800

    Spacing/doc

--- spec/support/shared_examples/shared_resource_controller_specs.rb
+++ spec/support/shared_examples/shared_resource_controller_specs.rb
@@ -1,8 +1,11 @@
+# These just make sure there are no blatant view errors.
+# Could definitely be faster and more useful.
+
 shared_examples_for "resource controller" do
   render_views
     
   before :each do
-    @object = create resource
+    @object     = create resource
     @admin_user = create :admin_user
     controller.stub(:admin_user) { @admin_user }
   end
