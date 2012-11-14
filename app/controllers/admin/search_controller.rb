2371c7e034fe0829f1763aeac5d0cad05808e641
Bryan Ricker <bricker88@gmail.com>
2012-11-13 19:08:58 -0800

    Un-deprecate search model

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -1,23 +1,27 @@
 class Admin::SearchController < Admin::BaseController
   before_filter :set_class
   
-  def index
-    flash[:info] = "Search functionality isn't implemented yet. Sorry!"
-    return
-    
+  def index    
     @records = @klass.search(params[:query],
       :page     => params[:page] || 1,
       :per_page => 50
     )
     
     @list = @klass.admin.list
+    render "/admin/resource/index"
   end
+
+  #--------------
   
+  helper_method :resource_class
   def resource_class
     @klass
   end
   
+  #--------------
+  
   private
+  
   def set_class
     @klass = AdminResource::Helpers::Naming.to_class(params[:resource])
   end
