9337915d84c9f1f33e7baceccb4a50cecc87c1b6
Bryan Ricker <bricker88@gmail.com>
2012-11-30 16:53:47 -0800

    Fix tests for category

--- spec/concerns/concern/associations/category_association_spec.rb
+++ spec/concerns/concern/associations/category_association_spec.rb
@@ -3,6 +3,5 @@ require "spec_helper"
 describe Concern::Associations::CategoryAssociation do
   subject { TestClass::Story.new }
   
-  it { should have_one(:content_category) }
-  it { should have_one(:category).through(:content_category) }
+  it { should belong_to(:category) }
 end
