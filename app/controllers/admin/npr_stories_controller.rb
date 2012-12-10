49950cf6ca70b39f53fb9a27cdd64af36ba84835
Bryan Ricker <bricker88@gmail.com>
2012-12-10 15:09:51 -0800

    Add npr stories controller

---
+++ app/controllers/admin/npr_stories_controller.rb
@@ -0,0 +1,40 @@
+class NprStoriesController < Admin::BaseController
+  include AdminResource::Controller::Helpers
+  include AdminResource::Controller::Callbacks
+
+  before_filter :authorize_resource  
+  before_filter :get_record, only: [:destroy]
+  before_filter :get_records, only: [:index]
+  before_filter :extend_breadcrumbs_with_resource_root
+
+  respond_to :html, :json, :js
+  
+  def index
+    @list = resource_class.admin.list
+    respond_with :admin, @records
+  end
+  
+  #--------------
+  
+  def destroy
+    @record.destroy
+    notice "Deleted #{@record.simple_title}"
+    respond_with :admin, @record
+  end
+
+  #--------------
+  
+  def extend_breadcrumbs_with_resource_root
+    breadcrumb resource_class.to_title.pluralize, resource_class.admin_index_path
+  end
+
+  #--------------
+  
+  private
+
+  #--------------
+  
+  def authorize_resource
+    authorize!(resource_class)
+  end
+end
