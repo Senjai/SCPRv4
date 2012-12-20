48af6837e46b24737feb72ffdd63a9a8e7e1cd84
Bryan Ricker <bricker88@gmail.com>
2012-12-19 17:24:27 -0800

    Updates to Newsroom::Hook

--- lib/admin_resource/hook.rb
+++ lib/admin_resource/hook.rb
@@ -5,25 +5,23 @@
 #
 module AdminResource
   class Hook
-    attr_accessor :action, :user, :data
+    attr_accessor :data, :path
     
     def initialize(options={})
-      @action = options[:action]
-      @user   = options[:user]
-      @data   = options[:data] || { touch: true }
+      @path = options[:path]
+      @data = options[:data] || { touch: true }
     end
     
     #--------------
     # Publish the message
     def publish
-      Net::HTTP.post_form(url, data)
+      Net::HTTP.post_form(URI.parse(url), data)
     end
 
     #--------------
     
     def url
-      uri = "#{Rails.application.config.node.server}/notify/#{@action}/#{@user}"
-      URI.parse(uri)
+      File.join(Rails.application.config.node.server, self.path)
     end
   end
 end
