acf80120f063e5feeaff94411a8d6dcb56b024ea
Bryan Ricker <bricker88@gmail.com>
2012-12-31 11:41:19 -0800

    sweet schema version bro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121221221537) do
+ActiveRecord::Schema.define(:version => 20121230124902) do
 
   create_table "about_town_feature", :force => true do |t|
     t.string   "slug",          :limit => 50,         :null => false
