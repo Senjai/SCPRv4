9e8318df8acdcf36bbfa6b3302807f065d852ab9
Bryan Ricker <bricker88@gmail.com>
2013-01-16 17:48:11 -0800

    Preview for Events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,9 @@
 class Admin::EventsController < Admin::ResourceController
+  def preview
+    @event = ContentBase.obj_by_key(params[:obj_key])
+    @event.assign_attributes(params[:event])
+    @title = @event.to_title
+    
+    render "/events/_event", layout: "/admin/preview", locals: { event: @event }
+  end
 end
