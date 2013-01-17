023f10f3ffb861650fe73935e3e0ad4e9d0c08e4
Bryan Ricker <bricker88@gmail.com>
2013-01-16 17:34:09 -0800

    Preview for Segments

--- app/controllers/admin/blog_entries_controller.rb
+++ app/controllers/admin/blog_entries_controller.rb
@@ -2,7 +2,6 @@ class Admin::BlogEntriesController < Admin::ResourceController
   def preview
     @entry = ContentBase.obj_by_key!(params[:obj_key])
     @entry.assign_attributes(params[:blog_entry])
-    
     @title = @entry.to_title
     
     render "/blogs/_entry", layout: "/admin/preview", locals: { entry: @entry, full: true }
