4e002e61718b95b7a17d7b951b6b15e4b992f173
Bryan Ricker <bricker88@gmail.com>
2013-01-18 13:58:43 -0800

    Remove old class

--- spec/fixtures/models/thing_with_assets.rb
+++
@@ -1,7 +0,0 @@
-module TestClass
-  class ThingWithAssets < ActiveRecord::Base
-    self.table_name = "test_class_thing_with_assets"
-    
-    include Concern::Associations::AssetAssociation
-  end
-end
