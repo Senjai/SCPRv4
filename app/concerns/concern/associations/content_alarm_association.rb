fdd7e7399fb44bdc923e5eea1de3d7f6628e8c71
Bryan Ricker <bricker88@gmail.com>
2013-01-23 17:08:13 -0800

    Refactor Publishing (WIP) TODO: Implement nested ContentAlarms for ContentShells, FeaturedComments

--- app/concerns/concern/associations/content_alarm_association.rb
+++ app/concerns/concern/associations/content_alarm_association.rb
@@ -4,6 +4,13 @@
 # Association definition for content_alarm
 # Also includes related callbacks
 #
+# It's important that content_alarm is a has_one association, because
+# this takes advantage of the fact that you can just save right over the old one.
+# We do this to keep the code simple - rather than trying to update the alarm, just
+# make a new one when the published_at date is changed.
+#
+# Required attributes: [:status, :pending?, :published_at, :unpublishing]
+#
 module Concern
   module Associations
     module ContentAlarmAssociation
@@ -11,25 +18,34 @@ module Concern
       
       included do
         has_one :alarm, as: :content, class_name: "ContentAlarm", dependent: :destroy
-        accepts_nested_attributes_for :alarm, reject_if: :should_reject_alarm?, allow_destroy: true
-        
+        before_save :generate_alarm, if: :should_generate_alarm?
         before_save :destroy_alarm, if: :should_destroy_alarm?
       end
-
-      #------------------
       
+      #------------------
+      # When to generate an alarm
+      def should_generate_alarm?
+        self.pending? && self.published_at_changed? && self.published_at.present?
+      end
+      
+      #------------------
       # If we're changing status from Pending to something else,
       # and there was an alarm, get rid of it.
       def should_destroy_alarm?
         self.alarm.present? && self.status_changed? && self.status_was == ContentBase::STATUS_PENDING
       end
       
-      def should_reject_alarm?(attributes)
-        attributes['fire_at'].blank?
-      end
-
       #------------------
-
+      # Build an alarm
+      # Double-check that published_at is present, otherwise errors.
+      def generate_alarm
+        if self.published_at.present?
+          self.build_alarm(fire_at: self.published_at)
+        end
+      end
+      
+      #------------------
+      # Mark the alarm for destruction
       def destroy_alarm
         self.alarm.mark_for_destruction
       end
--- spec/concerns/associations/content_alarm_association_spec.rb
+++ spec/concerns/associations/content_alarm_association_spec.rb
@@ -1,9 +1,34 @@
 require "spec_helper"
 
 describe Concern::Associations::ContentAlarmAssociation do
-  subject { TestClass::Story.new }
+  subject { build :test_class_story }
   
   it { should have_one(:alarm).class_name("ContentAlarm").dependent(:destroy) }
+  
+  #--------------------
+  
+  describe '#should_generate_alarm?' do
+    it "is true if the story is pending and published_at is present" do
+      subject.status = ContentBase::STATUS_PENDING
+      subject.published_at = Time.now.tomorrow
+      
+      subject.should_generate_alarm?.should eq true
+    end
+    
+    it 'is false if not pending' do
+      subject.status = ContentBase::STATUS_PUBLISHED
+      subject.published_at = Time.now.tomorrow
+      subject.should_generate_alarm?.should eq false
+    end
+    
+    it 'is false if published_at not present' do
+      subject.status = ContentBase::STATUS_PENDING
+      subject.published_at = nil
+      subject.should_generate_alarm?.should eq false
+    end
+  end
+  
+  #--------------------
   
   describe "#should_destroy_alarm?" do
     it "is true if there was an alarm and status went from pending to not pending" do
@@ -26,22 +51,33 @@ describe Concern::Associations::ContentAlarmAssociation do
       subject.should_destroy_alarm?.should eq false
     end
   end
-  
-  #---------------------
-  
-  describe "#should_reject_alarm?" do
-    it "is true if fire_at is blank" do
-      attributes = { 'fire_at' => "" }
-      subject.should_reject_alarm?(attributes).should eq true
-    end
-    
-    it "is false if fire_at is present" do
-      attributes = { 'fire_at' => "now" }
-      subject.should_reject_alarm?(attributes).should eq false
-    end
-  end
 
   #---------------------
+  
+  describe '#generate_alarm' do
+    before :each do
+      subject.status = ContentBase::STATUS_PENDING
+    end
+    
+    it "generates an alarm if published_at is present" do
+      subject.published_at = Time.now.tomomorrow
+      subject.generate_alarm
+      
+      subject.alarm.should be_a ContentBase::ContentAlarm
+      subject.alarm.fire_at.should eq subject.published_at
+      subject.alarm.persisted?.should eq false
+    end
+    
+    it "doesn't generate the alarm is published_at is blank" do
+      subject.published_at = nil
+      subject.generate_alarm
+      
+      subject.alarm.should eq nil
+    end
+  end
+  
+  
+  #--------------------
   
   describe "#destroy_alarm" do
     it "marks alarm for destruction" do
