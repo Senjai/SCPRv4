7b86d1983b68423f33892261543e5a312db2a8be
Bryan Ricker <bricker88@gmail.com>
2013-01-28 18:05:11 -0800

    Add activity page for each user

--- app/controllers/admin/admin_users_controller.rb
+++ app/controllers/admin/admin_users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::AdminUsersController < Admin::ResourceController
+class Admin::AdminUsersController < Admin::ResourceController  
   # Override this method from Admin::ResourceController
   # Users should always be able to see and update their
   # own profile.
@@ -8,5 +8,14 @@ class Admin::AdminUsersController < Admin::ResourceController
     end
     
     super
+  end
+  
+  def activity
+    get_record
+    breadcrumb @record.to_title, @record.admin_edit_path, "Activity"
+    
+    @list = Secretary::Version.admin.list
+    @versions = @record.activities.order(@list.order).page(params[:page]).per(@list.per_page)
+    render '/admin/versions/index'
   end
 end
