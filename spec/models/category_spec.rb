dec492304f7971f96f3ba55d410972f34afe5728
Bryan Ricker <bricker88@gmail.com>
2013-01-28 17:22:51 -0800

    Lots of test cleanup

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,19 +1,6 @@
 require "spec_helper"
 
-describe Category do
-  describe "validations" do
-    it_behaves_like "slug validation"
-    it { should validate_presence_of(:title) }
-  end
-  
-  #--------------------
-  
-  describe "associations" do
-    it { should belong_to(:comment_bucket).class_name("FeaturedCommentBucket") }
-  end
-
-  #--------------------
-  
+describe Category do  
   describe "content" do
     it "returns an empty array if the page * per_page is greater than Thinking Sphinx's max_matches" do
       category = create :category_news
--- spec/models/show_episode_spec.rb
+++ spec/models/show_episode_spec.rb
@@ -1,9 +1,7 @@
 require "spec_helper"
 
 describe ShowEpisode do
-  describe "callbacks" do
-    it_behaves_like "set published at callback"
-    
+  describe "callbacks" do    
     describe "generate_headline" do
       let(:program) { build :kpcc_program, title: "Cool Show" }
 
@@ -21,23 +19,9 @@ describe ShowEpisode do
     end
   end
   
-  # ----------------
-  
-  describe "associations" do
-    it_behaves_like "content alarm association"
-    it_behaves_like "asset association"
-    it_behaves_like "audio association"
-    
-    it { should have_many(:rundowns) }
-    it { should have_many(:segments).through(:rundowns) }
-    it { should belong_to(:show) }
-  end
-  
   #------------------
   
   describe "validations" do
-    it_behaves_like "content validation"
-
     it { should validate_presence_of(:show) }
     
     it "validates air date on publish" do
@@ -48,12 +32,10 @@ describe ShowEpisode do
 
   #------------------
   
-  describe "scopes" do
-    it_behaves_like "since scope"
-    
+  describe "scopes" do    
     describe "#published" do
       it "orders published content by air_date descending" do
-        episodes = create_list :show_episode, 3, status: 5
+        episodes = create_list :show_episode, 3, status: ContentBase::STATUS_LIVE
         ShowEpisode.published.first.should eq episodes.last
         ShowEpisode.published.last.should eq episodes.first
       end
@@ -62,19 +44,10 @@ describe ShowEpisode do
   
   #------------------
   
-  it_behaves_like "status methods"
-  it_behaves_like "publishing methods"
-  
-  describe "content base methods" do
-    it { should_not respond_to :disqus_identifier }
-  end
-  
-  #------------------
-  
   describe "#teaser" do
     it "is the body" do
-      show_episode = build :show_episode, body: load_fixture("long_text.txt")
+      show_episode = build :show_episode, body: "hello"
       show_episode.teaser.should eq show_episode.body
     end
-  end  
+  end
 end
