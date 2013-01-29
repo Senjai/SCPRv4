981974d8cf908e567fc5af2f9f3184187f788cc6
Bryan Ricker <bricker88@gmail.com>
2013-01-28 17:22:20 -0800

    Use factories

--- spec/concerns/concern/callbacks/generate_slug_callback_spec.rb
+++ spec/concerns/concern/callbacks/generate_slug_callback_spec.rb
@@ -2,22 +2,35 @@ require "spec_helper"
 
 describe Concern::Callbacks::GenerateSlugCallback do
   it "generates slug if it's blank by default" do
-    story = TestClass::Story.new(headline: "This is a ... WEIRD headline! And it's kind of long too, what's up with that?", body: "Cool Body", status: ContentBase::STATUS_LIVE)
+    story = build :test_class_story, headline: "This is a ... WEIRD headline! And it's kind of long too, what's up with that?", slug: nil
     story.save!
-    story.slug.should match /this-is-a-weird/
+    story.slug.should match /^this-is-a-weird/
     story.slug.length.should eq 50
   end
   
   it "chomps trailing hyphens" do
-    story = TestClass::Story.new(headline: "012345678901234567890123456789012345678901234567---", body: "Cool Body", status: ContentBase::STATUS_LIVE)
-    story.save!
+    story = build :test_class_story, headline: "012345678901234567890123456789012345678901234567---", slug: nil
+    story.generate_slug
     story.slug.should_not match /-$/
     story.slug.should match /7$/
   end
   
   it "doesn't generate slug if it already existed" do
-    story = TestClass::Story.new(slug: "test-test", headline: "This is a ... WEIRD headline! And it's kind of long too, what's up with that?", body: "Cool Body", status: ContentBase::STATUS_LIVE)
+    story = build :test_class_story, slug: "test-test", headline: "Sweet headline bro"
     story.save!
     story.slug.should eq "test-test"
+  end
+  
+  it "doesn't generate the slug if should_validate? is false" do
+    story = build :test_class_story, slug: nil, headline: "Sweet Headline"
+    story.stub(:should_validate?) { false }
+    story.save!
+    story.slug.should eq nil
+  end
+  
+  it "doesn't generate slug if headline is blank" do
+    story = build :test_class_story, headline: "", slug: nil
+    story.generate_slug
+    story.slug.should eq nil
   end
 end
--- spec/concerns/concern/scopes/published_scope_spec.rb
+++ spec/concerns/concern/scopes/published_scope_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 describe Concern::Scopes::PublishedScope do
   it "orders by published_at desc" do
-    story1 = TestClass::Story.create!(headline: "Headline", body: "Body", slug: "slug1", published_at: Time.now, status: ContentBase::STATUS_LIVE)
-    story3 = TestClass::Story.create!(headline: "Headline", body: "Body", slug: "slug3", published_at: Time.now+2.hour, status: ContentBase::STATUS_LIVE)
-    story2 = TestClass::Story.create!(headline: "Headline", body: "Body", slug: "slug2", published_at: Time.now+1.hour, status: ContentBase::STATUS_LIVE)
+    story1 = create :test_class_story, published_at: 3.day.ago
+    story2 = create :test_class_story, published_at: 2.days.ago
+    story3 = create :test_class_story, published_at: 1.days.ago
     TestClass::Story.published.should eq [story3, story2, story1]
     TestClass::Story.published.to_sql.should match /order by published_at desc/i
   end
   
   it "only grabs published content" do
-    story_published   = TestClass::Story.create!(headline: "Headline", body: "Body", slug: "slug1", published_at: Time.now, status: ContentBase::STATUS_LIVE)
-    story_unpublished = TestClass::Story.create!(headline: "Headline", body: "Body", slug: "slug2", published_at: Time.now, status: ContentBase::STATUS_DRAFT)
+    story_published   = create :test_class_story, status: ContentBase::STATUS_LIVE
+    story_unpublished = create :test_class_story, status: ContentBase::STATUS_DRAFT
     TestClass::Story.published.should eq [story_published]
   end
 end
