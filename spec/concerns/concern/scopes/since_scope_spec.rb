98b6c1bb7b9833197b67a5acb98e626dd235c7ba
Bryan Ricker <bricker88@gmail.com>
2013-01-31 23:07:48 -0800

    Fix spec

--- spec/concerns/concern/scopes/since_scope_spec.rb
+++ spec/concerns/concern/scopes/since_scope_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 describe Concern::Scopes::SinceScope do
   it "gets only the records from the specified limit" do
-    recent     = TestClass::Story.create(headline: "Headline", body: "Body", status: ContentBase::STATUS_LIVE, slug: "headline", published_at: 1.day.ago)
-    really_old = TestClass::Story.create(headline: "Other Headline", body: "Body", status: ContentBase::STATUS_LIVE, slug: "otherheadline", published_at: 1.year.ago)
+    recent     = create :test_class_story, published_at: 1.day.ago
+    really_old = create :test_class_story, published_at: 1.year.ago
 
     TestClass::Story.since(1.hour.ago).should eq []
     TestClass::Story.since(3.days.ago).should eq [recent]
