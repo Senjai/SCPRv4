0c14bbd8d78f110586670a0279957258bf20ee5e
Bryan Ricker <bricker88@gmail.com>
2013-02-05 23:23:20 -0800

    Manually report errors to NewRelic in production

--- app/concerns/concern/controller/custom_errors.rb
+++ app/concerns/concern/controller/custom_errors.rb
@@ -19,7 +19,7 @@ module Concern
       
       def render_error(status, e=Exception)
         render template: "/errors/error_#{status}", status: status, locals: { errors: e }
-        super
+        NewRelic::Agent.agent.error_collector.notice_error(e)
       end
     end # CustomErrors
   end # Controller
