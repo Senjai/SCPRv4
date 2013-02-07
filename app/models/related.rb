8c5c0db06a61152e36cb240ca7b65c15b8c7539e
Bryan Ricker <bricker88@gmail.com>
2013-02-07 14:51:32 -0800

    Restrict related content to only Published content

--- app/models/related.rb
+++ app/models/related.rb
@@ -2,8 +2,8 @@ class Related < ActiveRecord::Base
   self.table_name =  'media_related'
   
   map_content_type_for_django
-  belongs_to :content, polymorphic: true
-  belongs_to :related, polymorphic: true
+  belongs_to :content, polymorphic: true, conditions: { status: ContentBase::STATUS_LIVE }
+  belongs_to :related, polymorphic: true, conditions: { status: ContentBase::STATUS_LIVE }
   
   # Mapping for related content_type
   before_save :set_rel_django_content_type_id, on: :create, if: -> { self.rel_django_content_type_id.blank? }
