47e1e5776cedab4dc3f03a8710be6571534b1ec6
Bryan Ricker <bricker88@gmail.com>
2013-02-16 10:08:03 -0800

    Add back some files

---
+++ config/deploy/production.rb
@@ -0,0 +1,73 @@
+require 'new_relic/recipes'
+require 'net/http'
+
+# --------------
+# Variables
+set :branch, "master"
+set :rails_env, "production"
+
+# --------------
+# Roles
+web1  = "66.226.4.226"
+web2  = "66.226.4.227"
+web4  = "66.226.4.240"
+media = "66.226.4.228"
+
+role :app,      web1, web2, web4
+role :web,      web1, web2, web4
+role :workers,  media
+role :db,       web2, :primary => true
+role :sphinx,   media
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, roles: [:app, :workers] do
+    restart_file = "#{current_release}/tmp/restart.txt"
+    delay        = restart_delay.to_i
+
+    if delay <= 0
+      logger.info "Restarting application processes immediately on all servers..."
+      run "touch #{restart_file}"
+    else      
+      logger.info "Restarting application processes on each app server in #{restart_delay} second intervals..."
+      parallel(roles: [:app, :workers], pty: true, shell: false) do |session|
+        # Worker processes can be restarted immediately
+        session.when "in?(:workers)", "touch #{restart_file}"
+
+        # App processes should be staggered
+        find_servers(roles: [:app]).each_with_index do |server, i|
+          sleep_time = i * delay
+
+          touch_cmd   = "sleep #{sleep_time} && touch #{restart_file} && echo [`date`] Restarted application processes for #{server}"
+          restart_cmd = "nohup sh -c '(#{touch_cmd}) &' 2>&1 >> #{current_release}/log/restart.log"
+
+          session.when "server.host == '#{server.host}'", restart_cmd
+        end
+      end
+    end
+  end
+  
+  # --------------
+  
+  task :notify do
+    token = YAML.load_file Rails.root.join("config/api_config.yml")['production']['token']
+
+    data = {
+      :auth_token  => token,
+      :user        => `whoami`.gsub("\n", ""),
+      :datetime    => Time.now.strftime("%F %T"),
+      :application => application
+    }
+    
+    url = "http://www.scpr.org/dashboard/notify"
+    logger.info "Sending notification to #{url}"
+    Net::HTTP.post_form(URI.parse(URI.encode(url)), data)
+  end
+end
+
+
+# --------------
+# Callbacks
+before "deploy:update_code", "deploy:notify"
+after "deploy:update_code", "thinking_sphinx:configure"
+after "deploy:update", "newrelic:notice_deployment"
---
+++ config/environments/staging.rb
@@ -0,0 +1,92 @@
+Scprv4::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
+
+  # Code is not reloaded between requests
+  config.cache_classes = true
+
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local = true
+  
+  
+  # There's nothing expiring the cache on staging so don't use it
+  # If you change this to true, manually expire the cache
+  # by SSHing to the server and running `Rails.cache.clear`
+  # from the Rails console.
+  
+  # You can also switch this to true, change the Redis path to point to cache1,
+  # and change staging in database.yml to use mercer_new (production).
+  # That would be good for figuring out a problem that was only occurring
+  # in production.
+  
+  config.action_controller.perform_caching = false
+  config.cache_store = :redis_content_store, "redis://localhost:6379/6"
+
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
+
+  # Specifies the header that your server uses for sending files
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Enable Postmark for transactional mail sending
+  config.action_mailer.delivery_method          = :simple_postmark
+  config.action_mailer.simple_postmark_settings = { api_key: API_CONFIG['postmark']['api_key'] }
+  config.action_mailer.raise_delivery_errors    = true
+  
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+    
+  config.dbsync.filename    = "mercer.dump"
+  config.dbsync.local_dir   = "/web/scprv4/dbsync" # No trailing slash
+  config.dbsync.remote_host = "scprdb@66.226.4.229"
+  config.dbsync.remote_dir  = "~scprdb"
+  
+  config.scpr.host         = "staging.scprdev.org"
+  config.scpr.media_root   = "/home/kpcc/media"
+  config.scpr.media_url    = "http://media.scpr.org"
+  config.scpr.resque_queue = :scprv4
+  
+  config.node.server = "http://node.scprdev.org"
+  
+  config.assethost.server = "a.scpr.org"
+  config.assethost.token  = API_CONFIG['assethost']['token']
+  config.assethost.prefix = "/api"
+end
