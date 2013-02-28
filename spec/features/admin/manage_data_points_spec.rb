66ecd6fbd6173e0fcacd96b5dc3a5cedcb153d21
Bryan Ricker <bricker88@gmail.com>
2013-02-27 18:50:19 -0800

    Fix spec

--- spec/features/admin/manage_data_points_spec.rb
+++ spec/features/admin/manage_data_points_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe DataPoint do
   let(:valid_record) { build :data_point }
   let(:updated_record) { build :data_point }
-  let(:invalid_record) { build :data_point, data_key: nil }
+  let(:invalid_record) { build :data_point, title: "Cool Data Point", data_key: nil }
 
   it_behaves_like "managed resource create"
   it_behaves_like "managed resource destroy"
