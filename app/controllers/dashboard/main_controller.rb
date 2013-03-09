b4491d231e37e586343212c2011771f4606719c6
Bryan Ricker <bricker88@gmail.com>
2013-03-08 18:11:54 -0800

    Remove current_user, admin_user, and check_session methods

--- app/controllers/dashboard/main_controller.rb
+++ app/controllers/dashboard/main_controller.rb
@@ -1,6 +1,6 @@
 class Dashboard::MainController < ApplicationController
 
-  skip_before_filter :set_up_finders, :check_sessions, :add_params_for_newrelic, only: :notify
+  skip_before_filter :set_up_finders, :add_params_for_newrelic, only: :notify
   
   def notify
     if params[:auth_token] != Rails.application.config.api['assethost']['token']
