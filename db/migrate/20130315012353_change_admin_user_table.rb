d1c88d1c03c8c0f3d08b31e10eff9a3a7bf3b376
Bryan Ricker <bricker88@gmail.com>
2013-03-15 00:08:41 -0700

    Fix migration for admin user

--- db/migrate/20130315012353_change_admin_user_table.rb
+++ db/migrate/20130315012353_change_admin_user_table.rb
@@ -1,38 +1,41 @@
 class ChangeAdminUserTable < ActiveRecord::Migration
   def up
-    rename_table :auth_user, :admin_users
+    rename_column :auth_user, :password, :old_password
+    rename_column :auth_user, :is_staff, :can_login
 
-    rename_column :admin_users, :password, :old_password
-    rename_column :admin_users, :is_staff, :can_login
+    add_column :auth_user, :password_digest, :string
+    add_column :auth_user, :name, :string
 
-    add_column :admin_users, :password_digest, :string
-    add_column :admin_users, :name, :string
-
-    change_column :admin_users, :email, :string, null: true
+    change_column :auth_user, :email, :string, null: true
 
     AdminUser.all.each do |user|
-      user.name = "#{user.first_name} #{user.last_name}"
+      if user.first_name.blank? && user.last_name.blank?
+        user.name = "KPCC Employee"
+      else
+        user.name = [user.first_name, user.last_name].select { |n| n.present? }.join(" ")
+      end
+
       user.created_at = user.date_joined
       user.save!
     end
 
-    remove_column :admin_users, :first_name
-    remove_column :admin_users, :last_name
-    remove_column :admin_users, :date_joined
-    remove_column :admin_users, :is_active
+    remove_column :auth_user, :first_name
+    remove_column :auth_user, :last_name
+    remove_column :auth_user, :date_joined
+    remove_column :auth_user, :is_active
   end
 
   def down
-    rename_column :admin_users, :old_password, :password
-    rename_column :admin_users, :can_login, :is_staff
+    rename_column :auth_user, :old_password, :password
+    rename_column :auth_user, :can_login, :is_staff
 
-    remove_column :admin_users, :password_digest
-    remove_column :admin_users, :name
+    remove_column :auth_user, :password_digest
+    remove_column :auth_user, :name
 
-    add_column :admin_users, :first_name, :string
-    add_column :admin_users, :last_name, :string
-    add_column :admin_users, :date_joined, :datetime
-    add_column :admin_users, :is_active, :boolean, default: false
+    add_column :auth_user, :first_name, :string
+    add_column :auth_user, :last_name, :string
+    add_column :auth_user, :date_joined, :datetime
+    add_column :auth_user, :is_active, :boolean, default: false
 
     AdminUser.all.each do |user|
       names = user.name.split
@@ -41,7 +44,5 @@ class ChangeAdminUserTable < ActiveRecord::Migration
       user.date_joined = user.created_at
       user.save!
     end
-
-    rename_table :admin_users, :auth_user
   end
 end
