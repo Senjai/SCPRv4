5dfdb7efe96649d3211073e8966340611d172c80
Bryan Ricker <bricker88@gmail.com>
2013-03-15 11:56:37 -0700

    Remove some files moves into Outpost

--- spec/models/admin_user_permission_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-describe AdminUserPermission do
-  describe "associations" do
-    it { should belong_to(:admin_user) }
-    it { should belong_to(:permission) }
-  end
-end
