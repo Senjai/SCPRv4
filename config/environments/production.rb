9ad6f638b690ac771707b03f295a466c3303ad88
Bryan Ricker <bricker88@gmail.com>
2013-03-19 00:43:50 -0700

    Temporarily fallback to asset pipeline for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Scprv4::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.cache_store = :redis_content_store, "redis://10.226.4.234:6379/6"
+#  config.cache_store = :redis_content_store, "redis://localhost:6379/5"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -16,7 +17,7 @@ Scprv4::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
