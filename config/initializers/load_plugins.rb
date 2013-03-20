6828f47979d574c0477421ada10c15ee8a84c990
Bryan Ricker <bricker88@gmail.com>
2013-03-20 09:40:06 -0700

    Add a way to deprecate a magic database method

--- config/initializers/load_plugins.rb
+++ config/initializers/load_plugins.rb
@@ -2,6 +2,6 @@ require_dependency 'core_ext/time'
 require_dependency 'logs_as_task'
 require_dependency 'form_section'
 require_dependency 'acts_as_searchable'
-
+require_dependency 'replaced_method'
 # For testing purposes
 require "error_test"
