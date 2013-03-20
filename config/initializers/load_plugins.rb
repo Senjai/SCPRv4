456cffc5caf9d7f9d5a51bd5d1b1cc15c6a59446
Bryan Ricker <bricker88@gmail.com>
2013-03-20 09:47:43 -0700

    I am dumb

--- config/initializers/load_plugins.rb
+++ config/initializers/load_plugins.rb
@@ -2,7 +2,6 @@ require_dependency 'core_ext/time'
 require_dependency 'logs_as_task'
 require_dependency 'form_section'
 require_dependency 'acts_as_searchable'
-require_dependency 'replaced_attribute'
 
 # For testing purposes
 require "error_test"
