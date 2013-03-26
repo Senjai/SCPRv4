57a9deffe4f581866feddd4efdc0567c80441aea
Bryan Ricker <bricker88@gmail.com>
2013-03-26 12:02:57 -0700

    Make audio and missed it bucket dependent: :destroy

--- app/models/missed_it_bucket.rb
+++ app/models/missed_it_bucket.rb
@@ -11,7 +11,7 @@ class MissedItBucket < ActiveRecord::Base
   
   #--------------------
   # Association
-  has_many :contents, class_name: "MissedItContent", foreign_key: "bucket_id", order: "position asc"
+  has_many :contents, class_name: "MissedItContent", foreign_key: "bucket_id", order: "position asc", dependent: :destroy
   
   #--------------------
   # Validation
