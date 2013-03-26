8792a688292d7b5ea44080454039d359c2d903a6
Bryan Ricker <bricker88@gmail.com>
2013-03-26 15:05:12 -0700

    Make homepage status make more sense

--- app/models/homepage.rb
+++ app/models/homepage.rb
@@ -19,6 +19,23 @@ class Homepage < ActiveRecord::Base
   
   TEMPLATE_OPTIONS = TEMPLATES.map { |k, v| [v, k] }
   
+
+  STATUS_DRAFT    = ContentBase::STATUS_DRAFT
+  STATUS_PENDING  = ContentBase::STATUS_PENDING
+  STATUS_LIVE     = ContentBase::STATUS_LIVE
+
+  STATUS_TEXT = {
+    STATUS_DRAFT      => "Draft",
+    STATUS_PENDING    => "Pending",
+    STATUS_LIVE       => "Live"
+  }
+
+  class << self
+    def status_text_collect
+      Homepage::STATUS_TEXT.map { |p| [p[1], p[0]] }
+    end
+  end
+
   #-------------------
   # Scopes
   
@@ -29,7 +46,7 @@ class Homepage < ActiveRecord::Base
   
   #-------------------
   # Validations
-  validates :base, presence: true
+  validates :base, :status, presence: true
   
   #-------------------
   # Callbacks
