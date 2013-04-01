fc82db04e153becc4bb6fba83c170442cec1c579
Bryan Ricker <bricker88@gmail.com>
2013-04-01 16:04:58 -0700

    Remove modal_toggle helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,10 +1,2 @@
 module AdminHelper
-  #----------------
-  # Place a modal anywhere with a button to toggle it
-  #
-  # Takes a hash of options and a block with the content
-  #
-  def modal_toggle(options={}, &block)
-    render "/outpost/shared/modal", options: options, body: capture(&block)
-  end
 end
