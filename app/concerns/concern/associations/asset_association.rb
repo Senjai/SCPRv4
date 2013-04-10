46c4553dde1258ff59df75e1065952a3c434f6d8
Bryan Ricker <bricker88@gmail.com>
2013-04-10 13:29:18 -0700

    Move asset json logic out

--- app/concerns/concern/associations/asset_association.rb
+++ app/concerns/concern/associations/asset_association.rb
@@ -16,6 +16,8 @@ module Concern
           :dependent  => :destroy,
           :autosave   => true
         }
+
+        accepts_json_input_for_assets
       end
 
       #-------------------
@@ -23,67 +25,6 @@ module Concern
       # Returns nil if no assets are present.
       def primary_asset(size, format=:tag)
         self.assets.first.asset.send(size).send(format) if self.assets.present?
-      end
-      
-
-      # Define these methods manually since Rails uses a cache (not method_missing 
-      # directly) to call them, and we don't want (or need) to reset that.
-      def assets_changed?
-        attribute_changed?('assets')
-      end
-
-      #-------------------
-      # #asset_json is a way to pass in a string representation
-      # of a javascript object to the model, which will then be
-      # parsed and turned into ContentAsset objects in the 
-      # #asset_json= method.
-      def asset_json
-        current_assets_json.to_json
-      end
-
-      #-------------------
-      # Parse the input from #asset_json and turn it into real
-      # ContentAsset objects. 
-      def asset_json=(json)
-        # If this is literally an empty string (as opposed to an 
-        # empty JSON object, which is what it would be if there were no assets),
-        # then we can assume something went wrong and just abort.
-        # This shouldn't happen since we're populating the field in the template.
-        return if json.empty?
-
-        json = Array(JSON.parse(json)).sort_by { |c| c["position"].to_i }
-        loaded_assets = []
-        
-        json.each do |asset_hash|
-          new_asset = ContentAsset.new(
-            :asset_id   => asset_hash["id"].to_i,
-            :caption    => asset_hash["caption"].to_s,
-            :position   => asset_hash["position"].to_i
-          )
-          
-          loaded_assets.push new_asset
-        end
-        
-        loaded_assets_json = assets_to_simple_json(loaded_assets)
-
-        # If the assets didn't change, there's no need to bother the database.        
-        if current_assets_json != loaded_assets_json
-          self.changed_attributes['assets'] = current_assets_json
-          self.assets = loaded_assets
-        end
-
-        self.assets
-      end
-
-
-      private
-
-      def current_assets_json
-        assets_to_simple_json(self.assets)
-      end
-
-      def assets_to_simple_json(array)
-        Array(array).map(&:simple_json)
       end
     end # AssetAssociation
   end # Associations
--- app/models/content_asset.rb
+++ app/models/content_asset.rb
@@ -1,4 +1,6 @@
-class ContentAsset < ActiveRecord::Base  
+class ContentAsset < ActiveRecord::Base
+  include Outpost::AssetHost::JoinModelJson
+  
   self.table_name =  "assethost_contentasset"
   
   belongs_to :content, polymorphic: true
@@ -19,27 +21,5 @@ class ContentAsset < ActiveRecord::Base
     end
     
     return @_asset
-  end
-  
-  #----------
-  # Fetch asset JSON and then merge in our caption and position
-  def as_json(options={})
-    # grab asset as_json, merge in our values
-    self.asset.as_json(options).merge!({
-      "content_asset_id" => self.id.to_i,
-      "caption"          => self.caption.to_s, 
-      "ORDER"            => self.position.to_i, 
-      "credit"           => self.asset.owner.to_s
-    })
-  end
-
-  #----------
-  # This is for the asset manager
-  def simple_json
-    {
-      "id"          => self.asset_id,
-      "caption"     => self.caption,
-      "position"    => self.position.to_i
-    }
   end
 end
