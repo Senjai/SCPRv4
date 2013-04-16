5bc8691a60cf6a26aafb294220d26c200c1bbf49
Bryan Ricker <bricker88@gmail.com>
2013-04-16 11:35:57 -0700

    Rollback to not using ActsAsAlarm

--- app/models/content_alarm.rb
+++ app/models/content_alarm.rb
@@ -3,8 +3,26 @@ class ContentAlarm < ActiveRecord::Base
 
   self.table_name = "contentbase_contentalarm"
   logs_as_task
+
+  #----------
+  # Scopes
+  scope :pending, -> { where("fire_at <= ?", Time.now).order("fire_at") }
   
-  include Outpost::Publishing::ActsAsAlarm
+  #----------
+  # Association
+  belongs_to :content, polymorphic: true
+  
+  #---------------------
+  
+  class << self
+    #---------------------
+    # Fire any pending alarms
+    def fire_pending
+      self.pending.each do |alarm|
+        alarm.fire
+      end
+    end
+  end
 
   #---------------------
   # Fire an alarm.
@@ -26,6 +44,11 @@ class ContentAlarm < ActiveRecord::Base
   
   add_transaction_tracer :fire, category: :task
   
+  #---------------------
+  
+  def pending?
+    self.fire_at <= Time.now
+  end
 
   #---------------------
   # Can fire if this alarm is pending, and if the content is 
