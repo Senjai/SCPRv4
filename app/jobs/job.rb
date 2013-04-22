ff50908e88dab94109b7f846dd58623d3b461aa1
Bryan Ricker <bricker88@gmail.com>
2013-04-22 09:19:13 -0700

    Add newrelic instrumentation and logging for Job::Base

--- app/jobs/job.rb
+++ app/jobs/job.rb
@@ -3,9 +3,29 @@
 #
 module Job
   class Base
+    include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
     extend LogsAsTask
     logs_as_task
+
     
+    def log(message)
+      message = "*** #{message}"
+      
+      # Rails log and custom log always gets it
+      Rails.logger.info message
+      logger.info("***[#{Time.now}] #{self.class.name}: #{message}")
+      
+      # STDOUT only gets it if requested
+      if @verbose
+        $stdout.puts message
+      end
+    end
+    
+    def logger
+      @logger ||= Logger.new(Rails.root.join("log", "jobs.log"))
+    end
+
+
     private
     
     def self.namespace
