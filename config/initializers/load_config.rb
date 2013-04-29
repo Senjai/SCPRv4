48e56ef7dec266f6fb6ceb385e4084570da1a605
Bryan Ricker <bricker88@gmail.com>
2013-04-29 07:10:09 -0700

    Remove old table from static tables

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -2,7 +2,7 @@ DFP_ADS = YAML.load_file("#{Rails.root}/config/dfp_ads.yml")
 
 RSS_SPEC           = { 'version' => '2.0', 'xmlns:dc' => "http://purl.org/dc/elements/1.1/", 'xmlns:atom' => "http://www.w3.org/2005/Atom" }
 SPHINX_MAX_MATCHES = 1000
-STATIC_TABLES      = %w{ rails_content_map permissions }
+STATIC_TABLES      = %w{ permissions }
 
 CONNECT_DEFAULTS = {
   :facebook      => "http://www.facebook.com/kpccfm",
