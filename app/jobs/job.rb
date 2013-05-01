acb6962cb0324214c39803b5dc2540773beabfe4
Bryan Ricker <bricker88@gmail.com>
2013-04-30 19:56:52 -0700

    Move logging into Job::Base

--- app/jobs/job.rb
+++ app/jobs/job.rb
@@ -4,32 +4,40 @@
 module Job
   class Base
     include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
-    extend LogsAsTask
-    logs_as_task
 
-    
-    def log(message)
-      message = "*** #{message}"
+
+    class << self
+      def namespace
+        Rails.application.config.scpr.resque_queue
+      end
+
+      #--------------------
+
+      def log(message)
+        message = "*** #{message}"
+        
+        # Rails log and custom log always gets it
+        Rails.logger.info message
+        logger.info("***[#{Time.now}] #{self.name}: #{message}")
+        
+        # STDOUT only gets it if requested
+        if @verbose
+          $stdout.puts message
+        end
+      end
+
+      #--------------------
       
-      # Rails log and custom log always gets it
-      Rails.logger.info message
-      logger.info("***[#{Time.now}] #{self.class.name}: #{message}")
-      
-      # STDOUT only gets it if requested
-      if @verbose
-        $stdout.puts message
+      def logger
+        @logger ||= Logger.new(Rails.root.join("log", "jobs.log"))
       end
     end
-    
-    def logger
-      @logger ||= Logger.new(Rails.root.join("log", "jobs.log"))
-    end
 
 
-    private
-    
-    def self.namespace
-      Rails.application.config.scpr.resque_queue
+    #--------------------
+
+    def log(*args)
+      self.class.log(*args)
     end
   end
 end
