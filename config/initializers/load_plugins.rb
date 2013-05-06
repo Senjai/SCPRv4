09e4cbfcc8cc9799845a3e65d09dee04849e00f3
Bryan Ricker <bricker88@gmail.com>
2013-05-06 13:48:37 -0700

    Load audio vision module on initialize

--- config/initializers/load_plugins.rb
+++ config/initializers/load_plugins.rb
@@ -1,6 +1,7 @@
 require_dependency 'core_ext/time'
 require_dependency 'logs_as_task'
 require_dependency 'form_section'
+require_dependency 'audio_vision'
 
 # For testing purposes
 require "error_test"
