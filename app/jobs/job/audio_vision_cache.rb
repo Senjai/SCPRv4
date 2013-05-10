5ceadbf00de8d85bebecab86e478a7b455fb1af3
Bryan Ricker <bricker88@gmail.com>
2013-05-09 17:23:24 -0700

    Update A/V Homepage module handling; Add specs

--- app/jobs/job/audio_vision_cache.rb
+++ app/jobs/job/audio_vision_cache.rb
@@ -3,13 +3,32 @@ module Job
     @queue = namespace
 
     def self.perform
-      billboard = AudioVision::Billboard.current
-      
-      if billboard.present?
-        posts = billboard.posts
+      current_billboard_key = "audiovision:current_billboard"
+      featured_post_key     = "scprv4:homepage:av-featured-post"
 
-        Rails.cache.write("audiovision:featured-posts", posts)
-        self.cache(posts.first, "/home/cached/audiovision", "views/home/audiovision", local: :post)
+      current_billboard = AudioVision::Billboard.current
+      cached_billboard  = Rails.cache.read(current_billboard_key)
+      current_featured  = Rails.cache.read(featured_post_key)
+
+      if current_billboard
+        # When this task gets run:
+        #
+        # * If the updated timestamp of the billboard
+        #   has changed, then use the first post.
+        #
+        # * Otherwise, use a random post that isn't
+        #   the currently featured post.
+        #
+        if !cached_billboard || current_billboard.updated_at > cached_billboard.updated_at
+          featured = current_billboard.posts.first
+        else
+          featured = current_billboard.posts.select { |p| p.id != current_featured.id }.sample
+        end
+
+        Rails.cache.write(current_billboard_key, current_billboard)
+        Rails.cache.write(featured_post_key, featured)
+
+        self.cache(featured, "/home/cached/audiovision", "views/home/audiovision", local: :post)
       end
     end
   end
--- spec/jobs/job/audio_vision_cache_spec.rb
+++ spec/jobs/job/audio_vision_cache_spec.rb
@@ -1,5 +1,54 @@
 require 'spec_helper'
 
 describe Job::AudioVisionCache do
-  pending
+  describe '::perform' do
+    it 'caches the first post if the billboard has not been cached yet' do
+      FakeWeb.register_uri(:get, %r{audiovision\.scpr\.org/api/v1/billboards/current},
+        content_type: 'application/json', body: load_fixture('audiovision/billboard1_v1.json'))
+
+      Job::AudioVisionCache.perform
+      Rails.cache.read("scprv4:homepage:av-featured-post").should eq AudioVision::Billboard.current.posts.first
+    end
+
+
+    it "caches the first post if the billboard's updated_timestamp has changed" do
+      # Sanity Check
+      FakeWeb.register_uri(:get, %r{audiovision\.scpr\.org/api/v1/billboards/current},
+        content_type: 'application/json', body: load_fixture('audiovision/billboard1_v1.json'))
+
+      Job::AudioVisionCache.perform
+      Rails.cache.read("scprv4:homepage:av-featured-post").title.should match /Big Cats/
+
+      # Now test that it will change
+      FakeWeb.register_uri(:get, %r{audiovision\.scpr\.org/api/v1/billboards/current},
+        content_type: 'application/json', body: load_fixture('audiovision/billboard1_updated_v1.json'))
+
+      Job::AudioVisionCache.perform
+      Rails.cache.read("scprv4:homepage:av-featured-post").title.should match /Color of Space/
+    end
+
+
+    it "caches a different post if the updated timestamp has not changed" do
+      FakeWeb.register_uri(:get, %r{audiovision\.scpr\.org/api/v1/billboards/current},
+        content_type: 'application/json', body: load_fixture('audiovision/billboard1_v1.json'))
+
+      Job::AudioVisionCache.perform
+      # Sanity Check
+      featured = Rails.cache.read("scprv4:homepage:av-featured-post")
+      featured.title.should match /Big Cats/ # First post
+
+      last_title = featured.title
+
+      # Test that it will change.
+      # Do it 10 times so we can be reasonably sure
+      # that it never chooses the same post twice in 
+      # a row.
+      10.times do
+        Job::AudioVisionCache.perform
+        featured = Rails.cache.read("scprv4:homepage:av-featured-post")
+        featured.title.should_not eq last_title
+        last_title = featured.title
+      end
+    end
+  end
 end
