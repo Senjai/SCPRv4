167a8189c51673dfe6fbf954d1e5ced310e26da7
Bryan Ricker <bricker88@gmail.com>
2013-05-15 13:28:09 -0700

    Update the script to allow for non-bio bylines

--- util/scripts/csv_generation/multiple_stories_by_byline.rb
+++ util/scripts/csv_generation/multiple_stories_by_byline.rb
@@ -7,35 +7,36 @@ Erika Aguilar: 2012-12-01 - 2013-04-30
 Leslie Berestein Rojas: 2012-12-01 - 2013-04-30
 Ruxandra Guidi: 2012-12-01 - 2013-04-30
 Rina Palta: 2012-12-01 - 2013-04-30
-Alex Schaffert: 2012-12-01 - 2013-04-30
+Alex Schaffert-Callaghan: 2012-12-01 - 2013-04-30
 Evelyn Larrubia: 2012-12-01 - 2013-04-30
 Mae Ryan: 2012-12-01 - 2013-04-30
 Maya Sugarman: 2012-12-01 - 2013-04-30
 Ashley Alvarado: 2012-12-01 - 2013-04-30
 Leo Duran: 2012-12-01 - 2013-04-30
-Stephen Hoffman: 2012-12-01 - 2013-04-30
-Josie Huang: 2012-12-01 - 2013-04-30
+Josie  Huang: 2012-12-01 - 2013-04-30
 Laura Krantz: 2012-12-01 - 2013-04-30
 Michelle Lanz: 2012-12-01 - 2013-04-30
 Jacob Margolis: 2012-12-01 - 2013-04-30
-Steve Martin: 2012-12-01 - 2013-04-30
 A Martinez: 2012-12-01 - 2013-04-30
-Charles Castaldi: 2012-02-01 - 2013-04-30
 Adolfo Guzman-Lopez: 2012-02-01 - 2013-04-30
 Vanessa Romo: 2012-12-01 - 2013-03-31
 Kim Bui: 2012-12-01 - 2013-03-31
 Tami Abdollah: 2012-12-01 - 2012-12-15
 Cheryl Devall: 2012-12-01 - 2013-02-19
 Nick Roman: 2012-03-01 - 2013-04-30
-Kristen Lepore: 2013-04-01 - 2013-04-30
 EOS
+
+extra_names = [
+  { name: "Charles Castaldi", low: Time.parse("2012-02-01"), high: Time.parse("2013-04-30") },
+  { name: "Kristen Lepore", low: Time.parse("2012-04-01"), high: Time.parse("2013-04-30") }
+]
 
 # Classes to search for content
 classes = ["NewsStory", "BlogEntry", "ShowSegment"]
 
 # File name to dump the data into
 # Set :username for interpolation
-file_name = "stories-by-byline-#{Time.now.to_i}.csv"
+file_name = "stories-by-byline-#{Time.now.strftime("%F")}.csv"
 
 
 #----------------------------
@@ -64,6 +65,17 @@ user_ranges.each do |range|
     rows << [content.published_at, content.to_title, "http://scpr.org#{content.link_path}", content.byline]
   end
 end
+
+
+extra_names.each do |range|
+  bylines = ContentByline.where('name like ?', "%#{range[:name]}%").where(content_type: classes)
+
+  bylines.select { |b| b.content.published? && b.content.published_at.between?(range[:low], range[:high]) }.each do |byline|
+    content = byline.content
+    rows << [content.published_at, content.to_title, "http://scpr.org#{content.link_path}", content.byline]
+  end
+end
+
 
 CSV.open(Rails.root.join("log", file_name), "w+", headers: true) do |csv|
   csv << [
