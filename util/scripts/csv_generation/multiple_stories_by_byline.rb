f8462bf8c3045ad9c562419931bf1d8cb32ad32b
Bryan Ricker <bricker88@gmail.com>
2013-05-14 23:54:43 -0700

    Remove "smart" dashes
    "smart".

--- util/scripts/csv_generation/multiple_stories_by_byline.rb
+++ util/scripts/csv_generation/multiple_stories_by_byline.rb
@@ -3,30 +3,30 @@ require "csv"
 # Format is:
 # Jane Doe: YYYY-MM-DD - YYYY-MM-DD
 str = <<-EOS
-Erika Aguilar: 2012-12-01 – 2013-04-30
-Leslie Berestein Rojas: 2012-12-01 – 2013-04-30
-Ruxandra Guidi: 2012-12-01 – 2013-04-30
-Rina Palta: 2012-12-01 – 2013-04-30
-Alex Schaffert: 2012-12-01 – 2013-04-30
-Evelyn Larrubia: 2012-12-01 – 2013-04-30
-Mae Ryan: 2012-12-01 – 2013-04-30
-Maya Sugarman: 2012-12-01 – 2013-04-30
-Ashley Alvarado: 2012-12-01 – 2013-04-30
-Leo Duran: 2012-12-01 – 2013-04-30
-Stephen Hoffman: 2012-12-01 – 2013-04-30
-Josie Huang: 2012-12-01 – 2013-04-30
-Laura Krantz: 2012-12-01 – 2013-04-30
-Michelle Lanz: 2012-12-01 – 2013-04-30
-Jacob Margolis: 2012-12-01 – 2013-04-30
-Steve Martin: 2012-12-01 – 2013-04-30
-A Martinez: 2012-12-01 – 2013-04-30
-Charles Castaldi: 2012-02-01 – 2013-04-30
-Adolfo Guzman-Lopez: 2012-02-01 – 2013-04-30
-Vanessa Romo: 2012-12-01 – 2013-03-31
-Kim Bui: 2012-12-01 – 2013-03-31
+Erika Aguilar: 2012-12-01 - 2013-04-30
+Leslie Berestein Rojas: 2012-12-01 - 2013-04-30
+Ruxandra Guidi: 2012-12-01 - 2013-04-30
+Rina Palta: 2012-12-01 - 2013-04-30
+Alex Schaffert: 2012-12-01 - 2013-04-30
+Evelyn Larrubia: 2012-12-01 - 2013-04-30
+Mae Ryan: 2012-12-01 - 2013-04-30
+Maya Sugarman: 2012-12-01 - 2013-04-30
+Ashley Alvarado: 2012-12-01 - 2013-04-30
+Leo Duran: 2012-12-01 - 2013-04-30
+Stephen Hoffman: 2012-12-01 - 2013-04-30
+Josie Huang: 2012-12-01 - 2013-04-30
+Laura Krantz: 2012-12-01 - 2013-04-30
+Michelle Lanz: 2012-12-01 - 2013-04-30
+Jacob Margolis: 2012-12-01 - 2013-04-30
+Steve Martin: 2012-12-01 - 2013-04-30
+A Martinez: 2012-12-01 - 2013-04-30
+Charles Castaldi: 2012-02-01 - 2013-04-30
+Adolfo Guzman-Lopez: 2012-02-01 - 2013-04-30
+Vanessa Romo: 2012-12-01 - 2013-03-31
+Kim Bui: 2012-12-01 - 2013-03-31
 Tami Abdollah: 2012-12-01 - 2012-12-15
-Cheryl Devall: 2012-12-01 – 2013-02-19
-Nick Roman: 2012-03-01 – 2013-04-30
+Cheryl Devall: 2012-12-01 - 2013-02-19
+Nick Roman: 2012-03-01 - 2013-04-30
 Kristen Lepore: 2013-04-01 - 2013-04-30
 EOS
 
