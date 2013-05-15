fcd2c6b852d656bf547190569b5dd1fc05a6b580
Bryan Ricker <bricker88@gmail.com>
2013-05-15 14:40:19 -0700

    Revert "Remove special elections page"
    This reverts commit dc26acfb67ce66ebdd999883b8e491d024c3ca1e.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Scprv4::Application.routes.draw do
   # Listen Live
   get '/listen_live/' => 'listen#index', as: :listen
   
+  # Elections
+
+  # March Elections
+  match '/elections/2013/los-angeles-mayor-2013/' => 'specials#march2013elections'
+
   # Sections
   match '/category/carousel-content/:object_class/:id' => 'category#carousel_content',  as: :category_carousel, defaults: { format: :js }
   get '/news/'                                       => 'category#news',              as: :latest_news
