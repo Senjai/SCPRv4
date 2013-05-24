4bfcb8323a52fc49c78044942596b8e3742f3c9f
Bryan Ricker <bricker88@gmail.com>
2013-05-24 10:49:49 -0700

    Fix migration

--- db/migrate/20130523163512_remove_video_shell_permission.rb
+++ db/migrate/20130523163512_remove_video_shell_permission.rb
@@ -1,6 +1,8 @@
 class RemoveVideoShellPermission < ActiveRecord::Migration
   def up
-    Permission.find_by_resource!("VideoShell").destroy
+    permission = Permission.find_by_resource!("VideoShell")
+    Permission.delete(permission.id)
+    UserPermission.where(id: permission.id).delete_all
   end
 
   def down
