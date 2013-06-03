38dd32f08a3f4daf56a7d5ceb16b84547597ab37
Bryan Ricker <bricker88@gmail.com>
2013-06-03 16:42:05 -0700

    Add render_json method for rendering json.builder templates

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,6 @@
 module AdminHelper
+  # Pass in a path to the JSON file, get back pure JSON
+  def render_json(path, locals={})
+    raw(j(render(partial: path, formats: [:json], locals: locals)))
+  end
 end
