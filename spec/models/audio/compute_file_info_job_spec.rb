1452fb06ad5206e65f31c49613ef83184d44794b
Bryan Ricker <bricker88@gmail.com>
2013-06-03 14:05:03 -0700

    Fix a spec

--- spec/models/audio/compute_file_info_job_spec.rb
+++ spec/models/audio/compute_file_info_job_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe Audio::ComputeFileInfoJob do
   describe "::perform" do
     it "finds the audio and sends to Audio#compute_file_info!" do
+      purge_uploaded_audio
+      
       audio = create :audio, :uploaded
       Audio.should_receive(:find).with(audio.id).and_return(audio)
       audio.should_receive(:compute_file_info!)
