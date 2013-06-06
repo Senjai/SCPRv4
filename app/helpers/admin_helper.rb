b49c483255c6f6b7f617d06034a3a2bb414727e4
Bryan Ricker <bricker88@gmail.com>
2013-06-06 12:20:45 -0700

    Update outpost; remove render_json method.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,2 @@
 module AdminHelper
-  # Pass in a path to the JSON file, get back pure JSON
-  def render_json(path, locals={})
-    raw(j(render(partial: path, formats: [:json], locals: locals)))
-  end
 end
