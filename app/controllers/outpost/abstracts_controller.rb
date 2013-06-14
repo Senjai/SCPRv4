cae3c16fd4cf0b92e425790d7b5c8a49add21758
Bryan Ricker <bricker88@gmail.com>
2013-06-13 17:10:37 -0700

    Shorter column header

--- app/controllers/outpost/abstracts_controller.rb
+++ app/controllers/outpost/abstracts_controller.rb
@@ -8,7 +8,7 @@ class Outpost::AbstractsController < Outpost::ResourceController
     l.column :headline
     l.column :source
     l.column :url, display: :display_link
-    l.column :article_published_at, sortable: true, default_sort_mode: "desc"
+    l.column :article_published_at, header: "Published At", sortable: true, default_sort_mode: "desc"
     l.column :updated_at, header: "Last Updated", sortable: true, default_sort_mode: "desc"
 
     l.filter :source, collection: -> { Abstract.sources_select_collection }
