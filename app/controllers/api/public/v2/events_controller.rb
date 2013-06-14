cbc7069a4440f7cd549e60d3435dfe7b4c350627
Bryan Ricker <bricker88@gmail.com>
2013-06-14 13:10:51 -0700

    Begin setting up date filtering for events API

--- app/controllers/api/public/v2/events_controller.rb
+++ app/controllers/api/public/v2/events_controller.rb
@@ -1,8 +1,17 @@
 module Api::Public::V2
   class EventsController < BaseController
+    DEFAULTS = {
+      :limit => 40,
+      :page  => 1
+    }
+
+    MAX_RESULTS = 40
+
     before_filter \
+      :set_conditions,
       :sanitize_limit,
       :sanitize_page,
+      :sanitize_date_range,
       only: [:index]
 
     before_filter :sanitize_id, only: [:show]
@@ -10,7 +19,12 @@ module Api::Public::V2
     #---------------------------
     
     def index
-      @events = Event.order("created_at desc").page(@page).per(@limit)
+      @events = Event.order("starts_at").page(@page).per(@limit)
+      
+      @conditions.each do |condition|
+        @events = @events.where(condition)
+      end
+      
       respond_with @events
     end
     
@@ -30,12 +44,32 @@ module Api::Public::V2
 
     private
 
+    def set_conditions
+      @conditions = []
+    end
+
+    def sanitize_date
+      begin
+        start_date = \
+          params[:start_date] ? Time.parse(params[:start_date]) : Time.now
+
+        @conditions << ["start_date => ?", start_date]
+
+        if params[:end_date]
+          @conditions << ["end_date < ?", Time.parse(params[:end_date])]
+        end
+
+      rescue ArgumentError # Time couldn't be parsed
+        render_bad_request(message: "Invalid Date. Format is YYYY-MM-DD.") and return false
+      end
+    end
+
     #---------------------------
     # Limit to 40 for public API
     def sanitize_limit
       if params[:limit].present?
         limit = params[:limit].to_i
-        @limit = limit > 40 ? 40 : limit
+        @limit = limit > MAX_RESULTS ? MAX_RESULTS : limit
       else
         @limit = 10
       end
@@ -45,7 +79,7 @@ module Api::Public::V2
     
     def sanitize_page
       page = params[:page].to_i
-      @page = page > 0 ? page : 1
+      @page = page > 0 ? page : DEFAULTS[:page]
     end
     
     #---------------------------
