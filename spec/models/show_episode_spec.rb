cedb531d75b9a58733a4b189cabc9a2d9b8d1ebd
Bryan Ricker <bricker88@gmail.com>
2013-06-13 17:09:46 -0700

    Fix ShowEpisode spec

--- spec/models/show_episode_spec.rb
+++ spec/models/show_episode_spec.rb
@@ -53,56 +53,56 @@ describe ShowEpisode do
 
   #------------------
 
-  describe '#rundown_json' do
+  describe '#rundowns_json' do
     it "uses simple_json for the join model" do
       episode = create :show_episode
       segment = create :show_segment
       rundown = episode.rundowns.build(segment: segment, position: 0)
       rundown.save!
 
-      episode.rundown_json.should eq [rundown.simple_json].to_json
+      episode.rundowns_json.should eq [rundown.simple_json].to_json
       episode.segments.should eq [segment]
     end
   end
 
   #------------------
 
-  describe '#rundown_json=' do
+  describe '#rundowns_json=' do
     let(:episode)  { create :show_episode }
     let(:segment1) { create :show_segment }
     let(:segment2) { create :show_segment }
 
 
     it "adds them ordered by position" do
-      episode.rundown_json = "[{ \"id\": \"#{segment2.obj_key}\", \"position\": 1 }, {\"id\": \"#{segment1.obj_key}\", \"position\": 0 }]"
+      episode.rundowns_json = "[{ \"id\": \"#{segment2.obj_key}\", \"position\": 1 }, {\"id\": \"#{segment1.obj_key}\", \"position\": 0 }]"
       episode.segments.should eq [segment1, segment2]
     end
     
     it "parses the json and sets the content" do
       episode.segments.should be_empty
-      episode.rundown_json = "[{\"id\": \"#{segment1.obj_key}\", \"position\": 0 }, { \"id\": \"#{segment2.obj_key}\", \"position\": 1 }]"
+      episode.rundowns_json = "[{\"id\": \"#{segment1.obj_key}\", \"position\": 0 }, { \"id\": \"#{segment2.obj_key}\", \"position\": 1 }]"
       episode.segments.should eq [segment1, segment2]
     end
     
     it 'does not do anything if json is an empty string' do
       episode.segments.should be_empty
-      episode.rundown_json = "[{\"id\": \"#{segment1.obj_key}\", \"position\": 0 }, { \"id\": \"#{segment2.obj_key}\", \"position\": 1 }]"
+      episode.rundowns_json = "[{\"id\": \"#{segment1.obj_key}\", \"position\": 0 }, { \"id\": \"#{segment2.obj_key}\", \"position\": 1 }]"
       episode.segments.should_not be_empty
       
-      episode.rundown_json = ""
+      episode.rundowns_json = ""
       episode.segments.should_not be_empty
       
-      episode.rundown_json = "[]"
+      episode.rundowns_json = "[]"
       episode.segments.should be_empty
     end
 
     context "when no content has changed" do
       it "doesn't set the rundowns" do
         original_json = "[{ \"id\": \"#{segment1.obj_key}\", \"position\": 1 }]"
-        record = create :show_episode, rundown_json: original_json
+        record = create :show_episode, rundowns_json: original_json
 
         record.should_not_receive :rundowns=
-        record.rundown_json = original_json
+        record.rundowns_json = original_json
       end
     end
   end
