ad612a09bf170ad6232213d978fd5d7237d97b94
Bryan Ricker <bricker88@gmail.com>
2013-06-13 17:13:58 -0700

    Add spec for Edition#articles

--- spec/models/edition_spec.rb
+++ spec/models/edition_spec.rb
@@ -10,4 +10,14 @@ describe Edition do
       edition.abstracts.map(&:class).uniq.should eq [Abstract]
     end
   end
+
+  describe '#articles' do
+    it 'turns all of the items into articles' do
+      edition   = create :edition, :published
+      story     = create :news_story
+      slot      = create :edition_slot, edition: edition, item: story
+
+      edition.abstracts.map(&:class).uniq.should eq [Article]
+    end
+  end
 end
