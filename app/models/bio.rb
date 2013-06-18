7e3d96a728ec84f1a5772efdd6bd1dc6e48765a6
Bryan Ricker <bricker88@gmail.com>
2013-06-18 15:20:06 -0700

    Cleanup Bio#indexed_bylines

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -66,12 +66,15 @@ class Bio < ActiveRecord::Base
              .page(page).per(per_page)
     else
       ContentByline.search('', 
-        :classes => [NewsStory, BlogEntry, ShowSegment, ContentShell],
-        order:      :public_datetime,
-        sort_mode:  :desc,
-        with:       { user_id: self.id, status: ContentBase::STATUS_LIVE },
-        per_page:   per_page,
-        page:       page
+        :classes      => [NewsStory, BlogEntry, ShowSegment, ContentShell],
+        :order        => :public_datetime,
+        :sort_mode    => :desc,
+        :per_page     => per_page,
+        :page         => page,
+        :with         => { 
+          :user_id => self.id, 
+          :status  => ContentBase::STATUS_LIVE
+        }
       )
     end
   end
