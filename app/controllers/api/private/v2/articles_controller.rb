b7bb457cd0d7ce23c13905cad969c1d0d047b5be
Bryan Ricker <bricker88@gmail.com>
2013-06-18 14:52:44 -0700

    Doc

--- app/controllers/api/private/v2/articles_controller.rb
+++ app/controllers/api/private/v2/articles_controller.rb
@@ -131,6 +131,14 @@ module Api::Private::V2
 
     #---------------------------
     
+    # Hello. You've stumbled across this because you're 
+    # trying to figure out why unpublished content is 
+    # showing up in the aggregator. I'll tell you why.
+    #
+    # Remember that "true" and "false" parameters don't
+    # get converted to actual Ruby boolean values. 
+    # Use "1" and "0". Thinking Sphinx or ActiveRecord
+    # will convert them accordingly.
     def sanitize_conditions
       @conditions = params[:with]
     end
