795e074642588276a437271aca9c190d535185e0
Bryan Ricker <bricker88@gmail.com>
2013-06-21 14:18:18 -0700

    Remove old migration files

--- db/migrate/20130322175046_add_pij_to_events_and_segments.rb
+++
@@ -1,6 +0,0 @@
-class AddPijToEventsAndSegments < ActiveRecord::Migration
-  def change
-    add_column :shows_segment, :is_from_pij, :boolean
-    add_column :events, :is_from_pij, :boolean 
-  end
-end
