ef16e10514c793125cdf085fa18a1d738fd66cc8
Bryan Ricker <bricker88@gmail.com>
2013-06-24 12:34:00 -0700

    Cleanup

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -4,7 +4,7 @@
 FactoryGirl.define do
   factory :recurring_schedule_slot do
     sequence(:start_time) { |n| Time.new(2012, 10, 25, 1*n).second_of_week }
-    end_time { start_time + 2.hours }  
+    end_time { start_time + 2.hours }
     program { |f| f.association :kpcc_program }
   end
 
