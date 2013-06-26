465fadda7a44c3aa31038ec1c0667aa772fbabeb
Bryan Ricker <bricker88@gmail.com>
2013-06-26 13:59:47 -0700

    Fix schedule factory Time problem

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -3,8 +3,8 @@
 #
 FactoryGirl.define do
   factory :recurring_schedule_slot do
-    sequence(:start_time) { |n| Time.new(2012, 10, 25, 1*n).second_of_week }
-    end_time { start_time + 2.hours }
+    sequence(:start_time) { |n| (Time.now + n.hours).second_of_week }
+    end_time { start_time + 1.hour }
     program { |f| f.association :kpcc_program }
   end
 
