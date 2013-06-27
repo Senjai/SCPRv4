ecdb2bf0c6e1ca0bbe50662918faa0010d2059b6
Bryan Ricker <bricker88@gmail.com>
2013-06-27 16:43:10 -0700

    Set is_active to false for queries which were expired'

--- db/migrate/20130627232436_remove_expires_at_from_pij_queries.rb
+++ db/migrate/20130627232436_remove_expires_at_from_pij_queries.rb
@@ -1,5 +1,9 @@
 class RemoveExpiresAtFromPijQueries < ActiveRecord::Migration
   def up
+    PijQuery.where("expires_at is not null").each do |query|
+      query.update_column(:is_active, false)
+    end
+
     remove_column :pij_query, :expires_at
   end
 
