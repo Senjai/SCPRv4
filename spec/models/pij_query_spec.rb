26cf97e07984047b312642aa8b1f6f72beb12c93
Bryan Ricker <bricker88@gmail.com>
2013-06-28 15:02:32 -0700

    Remove some old specs

--- spec/models/pij_query_spec.rb
+++ spec/models/pij_query_spec.rb
@@ -1,42 +1,4 @@
 require "spec_helper"
 
 describe PijQuery do
-  describe "scopes" do
-    describe "evergreen" do
-      it "only selects evergreen" do
-        evergreen = create :pij_query, :evergreen, :visible
-        news      = create :pij_query, :news, :visible
-        PijQuery.evergreen.all.should eq [evergreen]
-      end
-    end
-
-    #---------------
-    
-    describe "news" do
-      it "only selects news" do
-        evergreen = create :pij_query, :evergreen, :visible
-        news      = create :pij_query, :news, :visible
-        PijQuery.news.all.should eq [news]
-      end
-    end
-    
-    #---------------
-    
-    describe "visible" do
-      it "doesn't select inactive" do
-        create :pij_query, :inactive
-        PijQuery.visible.should be_blank
-      end
-
-      it "doesn't select unpublished" do
-        create :pij_query, :unpublished
-        PijQuery.visible.should be_blank
-      end
-
-      it "selects anything which meets the criteria" do
-        active_query = create :pij_query, :visible
-        PijQuery.visible.should eq [active_query]
-      end
-    end
-  end  
 end
