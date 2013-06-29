e79973d43cbb48a9cd5d5e4ba3d5ef1d9d555ce1
Bryan Ricker <bricker88@gmail.com>
2013-06-28 17:15:01 -0700

    Add some pij query specs

--- spec/models/pij_query_spec.rb
+++ spec/models/pij_query_spec.rb
@@ -1,4 +1,24 @@
 require "spec_helper"
 
 describe PijQuery do
+  describe '#pending?' do
+    it 'checks if the status is pending' do
+      query = build :pij_query, status: PijQuery::STATUS_PENDING
+      query.pending?.should eq true
+    end
+  end
+
+  describe '#published?' do
+    it 'checks if the status is live' do
+      query = build :pij_query, status: PijQuery::STATUS_LIVE
+      query.published?.should eq true
+    end
+  end
+
+  describe '#to_article' do
+    it 'builds an article out of a pij query' do
+      query = build :pij_query
+      query.to_article.should be_a Article
+    end
+  end
 end
