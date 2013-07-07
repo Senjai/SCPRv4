fca61673c1d6cc3574ebddfe8f08c78db371a184
Bryan Ricker <bricker88@gmail.com>
2013-07-07 14:34:03 -0700

    Clean up form fields, clean days array in controller

--- app/controllers/outpost/recurring_schedule_rules_controller.rb
+++ app/controllers/outpost/recurring_schedule_rules_controller.rb
@@ -1,10 +1,19 @@
 class Outpost::RecurringScheduleRulesController < Outpost::ResourceController
   outpost_controller
+  before_filter :clean_days, only: [:create, :update]
 
   define_list do |l|
     l.per_page = 200
     
     l.column :program, display: ->(r) { r.program.title }
     l.column :schedule, header: "Rule", display: ->(r) { r.schedule.to_s }
+  end
+
+
+  private
+
+  def clean_days
+    params[:recurring_schedule_rule][:days] = 
+      params[:recurring_schedule_rule][:days].reject(&:blank?).map(&:to_i)
   end
 end
