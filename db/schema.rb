02c662e7770377e6e531ed0a1ab94afa19e3fd53
Bryan Ricker <bricker88@gmail.com>
2013-07-08 09:27:17 -0700

    Seed database for new schedule

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702182226) do
+ActiveRecord::Schema.define(:version => 20130708001126) do
 
   create_table "abstracts", :force => true do |t|
     t.string   "source"
