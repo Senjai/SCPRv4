9d1087962d2720a9fd08057d14d537e5241f45c8
Bryan Ricker <bricker88@gmail.com>
2013-07-08 17:28:35 -0700

    Better specs for PolymorphicProgramAssociation

--- spec/concerns/concern/associations/polymorphic_program_association_spec.rb
+++ spec/concerns/concern/associations/polymorphic_program_association_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Concern::Associations::PolymorphicProgramAssociation do
-  it 'sets the program based on the program_obj_key' do
-    program = create :kpcc_program
-    post = build :test_class_post
-    post.program.should eq nil
+  describe '#program_obj_key='
+    it 'sets the program based on the program_obj_key' do
+      program = create :kpcc_program
+      post = build :test_class_post
+      post.program.should eq nil
 
-    post.program_obj_key = program.obj_key
-    post.program.should eq program
+      post.program_obj_key = program.obj_key
+      post.program.should eq program
+    end
+  end
+
+  describe '#program_obj_key' do
+    it "is nil if there is no program" do
+      post = build :test_class_post
+      post.program_obj_key.should ewq nil
+    end
   end
 end
