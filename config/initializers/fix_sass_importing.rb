4339a51ec11e8839cf09ef692c04d7bec111a8ee
Bryan Ricker <bricker88@gmail.com>
2013-07-09 16:35:57 -0700

    Remove unneeded file

--- config/initializers/fix_sass_importing.rb
+++
@@ -1,6 +0,0 @@
-# https://github.com/rails/sass-rails/issues/119
-class Sass::Rails::Importer
-  def sass_file?(filename)
-    SASS_EXTENSIONS.keys.any? { |ext| filename.to_s.match(/#{ext}$/) }
-  end
-end
