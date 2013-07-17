e1baee9006202de6c6f2c68e593c18f8b17f79b6
Bryan Ricker <bricker88@gmail.com>
2013-07-16 17:12:58 -0700

    Setup external segment association mo' betta

--- app/models/external_segment.rb
+++ app/models/external_segment.rb
@@ -2,5 +2,10 @@ class ExternalSegment < ActiveRecord::Base
   include Concern::Associations::AudioAssociation
 
   belongs_to :external_program
-  belongs_to :external_episode
+  has_many :external_episode_segments
+  
+  has_many :external_episodes,
+    :through   => :external_episode_segments,
+    :dependent => :destroy
+
 end
