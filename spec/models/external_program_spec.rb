963923efee5d5b856a2c7cb4c673581db379af2b
Bryan Ricker <bricker88@gmail.com>
2013-07-22 16:32:31 -0700

    Fix specs

--- spec/models/external_program_spec.rb
+++ spec/models/external_program_spec.rb
@@ -4,10 +4,10 @@ describe ExternalProgram do
   describe "scopes" do
     describe "active" do
       it "selects programs with online or onair status" do
-        onair   = create :external_program, air_status: "onair"
-        online  = create :external_program, air_status: "online"
-        hidden  = create :external_program, air_status: "hidden"
-        archive = create :external_program, air_status: "archive"
+        onair   = create :external_program, :from_rss, air_status: "onair"
+        online  = create :external_program, :from_rss, air_status: "online"
+        hidden  = create :external_program, :from_rss, air_status: "hidden"
+        archive = create :external_program, :from_rss, air_status: "archive"
         ExternalProgram.active.sort.should eq [onair, online].sort
       end
     end
@@ -17,9 +17,9 @@ describe ExternalProgram do
 
   describe '#published?' do
     it "is true if air_status is not hidden" do
-      onair   = build :external_program, air_status: "onair"
-      online  = build :external_program, air_status: "online"
-      archive = build :external_program, air_status: "archive"
+      onair   = build :external_program, :from_rss, air_status: "onair"
+      online  = build :external_program, :from_rss, air_status: "online"
+      archive = build :external_program, :from_rss, air_status: "archive"
 
       onair.published?.should eq true
       online.published?.should eq true
@@ -27,7 +27,7 @@ describe ExternalProgram do
     end
 
     it "is false if air_status is hidden" do
-      hidden = build :external_program, air_status: "hidden"
+      hidden = build :external_program, :from_rss, air_status: "hidden"
       hidden.published?.should eq false
     end
   end
