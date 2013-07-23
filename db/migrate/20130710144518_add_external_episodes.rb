8a606b55e2439ae8916f29aa4c8d54f2bb15e316
Bryan Ricker <bricker88@gmail.com>
2013-07-23 16:47:37 -0700

    Remove external_url from episodes; update index

--- db/migrate/20130710144518_add_external_episodes.rb
+++ db/migrate/20130710144518_add_external_episodes.rb
@@ -3,14 +3,13 @@ class AddExternalEpisodes < ActiveRecord::Migration
     create_table :external_episodes do |t|
       t.string :title
       t.text :summary
-      t.string :external_url
-      t.string :external_id
       t.integer :external_program_id
+      t.string :external_id
       t.datetime :air_date
       t.timestamps
     end
 
-    add_index :external_episodes, :external_program_id
+    add_index :external_episodes, [:external_program_id, :external_id]
     add_index :external_episodes, :air_date
   end
 end
