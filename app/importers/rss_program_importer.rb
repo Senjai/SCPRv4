eea07a0cf55b4b29e4fd39beb4694dc98e331eb9
Bryan Ricker <bricker88@gmail.com>
2013-07-23 16:49:20 -0700

    Work on RSS Importer

--- app/importers/rss_program_importer.rb
+++ app/importers/rss_program_importer.rb
@@ -1,3 +1,9 @@
+# We look to the program's `feed_type` attribute to find out what we're
+# importing. Without this, we can't really know for sure whether each
+# RSS/podcast entry is a full episode (like BBC or Prarie Home Companion)
+# or a segment of an episode (Science Friday, Morning Edition).
+#
+#   
 module RssProgramImporter
   include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
 
@@ -13,16 +19,52 @@ module RssProgramImporter
     @external_program = external_program
   end
 
+  # We're only going to bother with the first 20 segments,
+  # because some feeds could have thousands of entries.
   def sync
-    Feedzirra::Feed.safe_fetch_and_parse(url) do |feed|
-      feed.entries.each do |entry|
+    feed = Feedzirra::Feed.safe_fetch_and_parse(url)
+
+    if @external_program.feed_type == "rss-episodes"
+      feed.entries.first(5).reject { |e| episode_exists?(e) }.each do |entry|
         @external_program.external_episodes.build(
-          :title      => entry.title,
-          :air_date   => entry.published
+          :title       => entry.title,
+          :summary     => entry.summary,
+          :air_date    => entry.published,
+          :external_id => entry.entry_id
+        )
+      end
+
+    elsif @external_program.feed_type == "rss-segments"
+      feed.entries.first(20).reject { |e| segment_exists?(e) }.each do |entry|
+        @external_program.external_segments.build(
+          :title          => entry.title,
+          :teaser         => entry.summary,
+          :published_at   => entry.published,
+          :external_id    => entry.entry_id,
+          :external_url   => entry.url
         )
       end
     end
+
+    @external_program.save!
   end
 
   add_transaction_tracer :sync, category: :task
+
+
+  private
+
+  def episode_exists?(entry)
+    ExternalEpisode.exists?(
+      :external_program_id    => @external_program.id,
+      :external_id            => entry.entry_id
+    )
+  end
+
+  def segment_exists?(entry)
+    ExternalSegment.exists?(
+      :external_program_id    => @external_program.id,
+      :external_id            => entry.entry_id
+    )
+  end
 end
