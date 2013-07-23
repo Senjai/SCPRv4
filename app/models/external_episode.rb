b71659368560cbbfe61651e281961232fcfa4670
Bryan Ricker <bricker88@gmail.com>
2013-07-22 17:51:33 -0700

    Add public_url to external episodes.

--- app/models/external_episode.rb
+++ app/models/external_episode.rb
@@ -1,7 +1,24 @@
 class ExternalEpisode < ActiveRecord::Base
+  # This also includes the admin routes, which we don't need.
+  # Really that should be split into two modules, PublicRouting and
+  # InternalRouting, or something.
+  include Outpost::Model::Routing
+
   include Concern::Associations::AudioAssociation
+  ROUTE_KEY = "episode"
 
   belongs_to :external_program
   has_many :external_episode_segments, order: "position"
   has_many :external_segments, through: :external_episode_segments
+
+  # This needs to match ShowEpisode
+  def route_hash
+    {
+      :show           => self.external_program.slug,
+      :year           => self.air_date.year,
+      :month          => "%02d" % self.air_date.month,
+      :day            => "%02d" % self.air_date.day,
+      :trailing_slash => true
+    }
+  end
 end
