4df8fd074e34c678e844fe09cf919248ce96b597
Bryan Ricker <bricker88@gmail.com>
2013-07-24 13:16:15 -0700

    Finish RSS importer (no audio import yet)

--- app/importers/rss_program_importer.rb
+++ app/importers/rss_program_importer.rb
@@ -1,10 +1,12 @@
+require 'rss'
+
 # We look to the program's `feed_type` attribute to find out what we're
 # importing. Without this, we can't really know for sure whether each
 # RSS/podcast entry is a full episode (like BBC or Prarie Home Companion)
 # or a segment of an episode (Science Friday, Morning Edition).
 #
 #   
-module RssProgramImporter
+class RssProgramImporter
   include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
 
 
@@ -22,26 +24,27 @@ module RssProgramImporter
   # We're only going to bother with the first 20 segments,
   # because some feeds could have thousands of entries.
   def sync
-    feed = Feedzirra::Feed.safe_fetch_and_parse(url)
+    feed = RSS::Parser.parse(@external_program.rss_url)
+    return false if !feed || feed.items.empty?
 
     if @external_program.feed_type == "rss-episodes"
-      feed.entries.first(5).reject { |e| episode_exists?(e) }.each do |entry|
+      feed.items.first(5).reject { |e| episode_exists?(e) }.each do |item|
         @external_program.external_episodes.build(
-          :title       => entry.title,
-          :summary     => entry.summary,
-          :air_date    => entry.published,
-          :external_id => entry.entry_id
+          :title       => item.title,
+          :summary     => item.description,
+          :air_date    => item.pubDate,
+          :external_id => item.guid.content
         )
       end
 
     elsif @external_program.feed_type == "rss-segments"
-      feed.entries.first(20).reject { |e| segment_exists?(e) }.each do |entry|
+      feed.items.first(20).reject { |e| segment_exists?(e) }.each do |item|
         @external_program.external_segments.build(
-          :title          => entry.title,
-          :teaser         => entry.summary,
-          :published_at   => entry.published,
-          :external_id    => entry.entry_id,
-          :external_url   => entry.url
+          :title          => item.title,
+          :teaser         => item.description,
+          :published_at   => item.pubDate,
+          :external_id    => item.guid.content,
+          :external_url   => item.link
         )
       end
     end
@@ -54,17 +57,17 @@ module RssProgramImporter
 
   private
 
-  def episode_exists?(entry)
+  def episode_exists?(item)
     ExternalEpisode.exists?(
       :external_program_id    => @external_program.id,
-      :external_id            => entry.entry_id
+      :external_id            => item.guid.content
     )
   end
 
-  def segment_exists?(entry)
+  def segment_exists?(item)
     ExternalSegment.exists?(
       :external_program_id    => @external_program.id,
-      :external_id            => entry.entry_id
+      :external_id            => item.guid.content
     )
   end
 end
--- spec/importers/rss_program_importer_spec.rb
+++ spec/importers/rss_program_importer_spec.rb
@@ -3,13 +3,35 @@ require 'spec_helper'
 describe RssProgramImporter do
   describe '::sync' do
     before :each do
-      FakeWeb.register_uri(:get, %r{bbc\.co\.uk},
+      FakeWeb.register_uri(:get, %r{californiareport},
         :content_type   => 'text/xml',
         :body           => load_fixture('rss/rss_feed.xml')
       )
     end
 
-    it "" do
+    it "imports segments if it's a segment feed" do
+      external_program = create :external_program, :from_rss, feed_type: "rss-segments"
+      RssProgramImporter.sync(external_program)
+      external_program.external_segments.should_not be_empty
+      external_program.external_episodes.should be_empty
+    end
+
+    it "imports episodes if it's an episode feed" do
+      external_program = create :external_program, :from_rss, feed_type: "rss-episodes"
+      RssProgramImporter.sync(external_program)
+      external_program.external_segments.should be_empty
+      external_program.external_episodes.should_not be_empty
+    end
+
+    it "doesn't import episodes that have already been imported" do
+      external_program = create :external_program, :from_rss, feed_type: "rss-episodes"
+      external_program.external_episodes.count.should eq 0
+      RssProgramImporter.sync(external_program)
+      count = external_program.external_episodes.count
+      count.should_not eq 0
+
+      RssProgramImporter.sync(external_program)
+      external_program.external_episodes.count.should eq count
     end
   end
 end
