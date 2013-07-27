8f06b595ecb273b439dd1b45a8947533be9ad2a0
Bryan Ricker <bricker88@gmail.com>
2013-07-26 17:06:49 -0700

    Also remove tmp upload dir

--- spec/support/audio_cleanup.rb
+++ spec/support/audio_cleanup.rb
@@ -1,5 +1,6 @@
 module AudioCleanup
   def purge_uploaded_audio
     FileUtils.rm_rf Rails.application.config.scpr.media_root.join("audio/upload")
+    FileUtils.rm_rf Rails.root.join("public", "uploads", "tmp")
   end
 end
