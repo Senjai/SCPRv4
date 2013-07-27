351df94a4ef2fc08ea661b359c3e799c8b0bc1e7
Bryan Ricker <bricker88@gmail.com>
2013-07-26 17:06:41 -0700

    Move file to correct location

--- app/models/audio/paths.rb
+++ app/models/audio/paths.rb
@@ -1,4 +1,4 @@
-module Audio
+class Audio
   module Paths
     #------------
     # The URL path, i.e. the path without "audio/"
@@ -11,25 +11,21 @@ module Audio
     # The server path, 
     # eg. /home/kpcc/media/audio/features/20121001_features999.mp3
     def full_path
-      @full_path ||= File.join Rails.application.config.scpr.media_root, "audio", self.path
+      @full_path ||= File.join(AUDIO_PATH_ROOT, self.path)
     end
 
     #------------
     # The full URL to the live audio,
     # eg. http://media.scpr.org/audio/upload/2012/10/01/your_sweet_audio.mp3
     def url
-      @url ||= begin
-        File.join(AUDIO_URL_ROOT, self.path) if self.live?
-      end
+      @url ||= File.join(AUDIO_URL_ROOT, self.path) if self.live?
     end
 
     #------------
     # The full URL to the live podcast audio,
     # eg. http://media.scpr.org/podcasts/airtalk/20120928_airtalk.mp3
     def podcast_url
-      @podcast_url ||= begin
-        File.join(PODCAST_URL_ROOT, self.path) if self.live?
-      end
+      @podcast_url ||= File.join(PODCAST_URL_ROOT, self.path) if self.live?
     end
   end
 end
