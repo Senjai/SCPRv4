94453502a59fcbab29d18ff479469b38ca89b2be
Bryan Ricker <bricker88@gmail.com>
2013-07-29 14:58:48 -0700

    Specs for direct audio

--- spec/models/audio/direct_audio_spec.rb
+++ spec/models/audio/direct_audio_spec.rb
@@ -1,4 +1,52 @@
 require "spec_helper"
 
 describe Audio::DirectAudio do
+  describe '#url' do
+    it "is just the specified external_url" do
+      audio = build :direct_audio, external_url: "http://scpr.org/wat.mp3"
+      audio.url.should eq "http://scpr.org/wat.mp3"
+    end
+  end
+
+  describe '#podcast_url' do
+    it "is just the specified external_url" do
+      audio = build :direct_audio, external_url: "http://scpr.org/wat.mp3"
+      audio.podcast_url.should eq "http://scpr.org/wat.mp3"
+    end
+  end
+
+  describe "setting the status" do
+    it "gets set to LIVE on save" do
+      audio = create :direct_audio, status: nil
+      audio.status.should eq Audio::STATUS_LIVE
+    end
+  end
+
+  describe '#path' do
+    it 'is nil' do
+      audio = build :direct_audio
+      audio.path.should eq nil
+    end
+  end
+
+  describe '#full_path' do
+    it 'is nil' do
+      audio = build :direct_audio
+      audio.full_path.should eq nil
+    end
+  end
+
+  describe '#store_dir' do
+    it 'is nil' do
+      audio = build :direct_audio
+      audio.store_dir.should eq nil
+    end
+  end
+
+  describe '#filename' do
+    it 'is is the filename' do
+      audio = build :direct_audio, external_url: "http://npr.org/audio/wat.mp3"
+      audio.filename.to_s.should eq "wat.mp3"
+    end
+  end
 end
