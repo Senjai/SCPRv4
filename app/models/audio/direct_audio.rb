55f8f8c9fe084888d60d999c14e2e1d5e6065942
Bryan Ricker <bricker88@gmail.com>
2013-07-29 14:54:08 -0700

    Fix stack overflow error

--- app/models/audio/direct_audio.rb
+++ app/models/audio/direct_audio.rb
@@ -11,6 +11,25 @@ class Audio
       end
     end
 
+    # Override these so it doesn't super up to Audio 
+    # and cause a stack overflow.
+    def path
+      nil
+    end
+
+    def full_path
+      nil
+    end
+
+    def store_dir
+      nil
+    end
+
+    def filename
+      @filename ||= Pathname.new(self.external_url).basename
+    end
+
+
     def url
       self.external_url
     end
