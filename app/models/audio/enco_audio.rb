24fd880b83fb8fbad33b3a2b017c54e904902fb3
Bryan Ricker <bricker88@gmail.com>
2013-07-29 14:58:37 -0700

    EncoAudio fixes

--- app/models/audio/enco_audio.rb
+++ app/models/audio/enco_audio.rb
@@ -6,9 +6,15 @@
 class Audio
   class EncoAudio < Audio
     include Audio::Paths
+    logs_as_task
 
     STORE_DIR       = "features"
     FILENAME_REGEX  = %r{(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})_features(?<enco_id>\d{4})\.mp3} # 20121001_features1809.mp3
+
+    SYNC_THRESHOLD = 2.weeks
+
+    before_save :set_status
+
 
     class << self
       include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
@@ -19,40 +25,48 @@ class Audio
 
 
       # This method is used by Job::SyncAudioJob
-      def bulk_sync(limit=nil)
-        limit ||= 2.weeks.ago
-        awaiting = self.awaiting_audio.where("created_at > ?", limit)
-        synced = 0
+      def bulk_sync
+        limit     = SYNC_THRESHOLD.ago
+        synced    = 0
+
+        awaiting  = self.awaiting_audio.where("created_at > ?", limit)
 
         if awaiting.empty?
-          self.log "No Audio to sync."
-        else
-
-          awaiting.each do |audio|
-            synced += 1 if audio.sync
-          end
-
-          self.log "Finished. Total synced: #{synced}"
+          log "No Audio to sync."
+          return false
         end
+
+        awaiting.each do |audio|
+          synced += 1 if audio.sync
+        end
+
+        log "Finished. Total synced: #{synced}"
       end
 
       add_transaction_tracer :bulk_sync, category: :task
     end # singleton
 
+
+
     def sync
       begin
         if File.exists? self.full_path
-          self.mp3 = self.full_path
-          audio.save!
-          self.log "Saved Audio ##{audio.id}: #{audio.full_path}"
+          self.mp3 = File.open(self.full_path)
+          self.save!
+
+          log "Saved Audio ##{self.id}: #{self.full_path}"
           self
         else
-          self.log "Still awaiting audio file for Audio ##{audio.id}: #{audio.full_path}"
+          log "Still awaiting audio file for Audio ##{self.id}: #{self.full_path}"
           false
         end
       rescue => e
-        self.log "Could not save Audio ##{audio.id}: #{e}"
-        false
+        if Rails.env.test?
+          raise e
+        else
+          log "Could not save Audio ##{self.id}: #{e}"
+          return false
+        end
       end
     end
 
@@ -64,6 +78,17 @@ class Audio
     def filename
       date = self.enco_date.strftime("%Y%m%d")
       "#{date}_features#{self.enco_number}.mp3"
+    end
+
+
+    private
+
+    def set_status
+      if self.mp3.present?
+        self.status = STATUS_LIVE
+      else
+        self.status = STATUS_WAIT
+      end
     end
   end # EncoAudio
 end # Audio
--- spec/models/audio/enco_audio_spec.rb
+++ spec/models/audio/enco_audio_spec.rb
@@ -1,41 +1,63 @@
 require "spec_helper"
 
 describe Audio::EncoAudio do
-  describe "::filename" do
-    it "is makes the filename based on enco number and date" do
-      audio = build :enco_audio, enco_number: "1234", enco_date: freeze_time_at("October 21, 1988")
-      Audio::EncoAudio.filename(audio).should eq "19881021_features1234.mp3"
+  describe "::bulk_sync" do
+    it "only syncs audio from the past 2 weeks by default" do
+      # Set mp3 to nil so we can test the sync process
+      old_enco   = create :enco_audio, :live_enco, mp3: nil
+      new_enco   = create :enco_audio, :live_enco, mp3: nil
+      old_enco.stub(:created_at) { 4.weeks.ago }
+
+      old_enco.mp3.file.should eq nil
+      new_enco.mp3.file.should eq nil
+
+      Audio::EncoAudio.bulk_sync
+
+      old_enco.mp3.file.should eq nil
+      new_enco.mp3.should_not eq nil
     end
   end
-  
-  #----------------
-  
-  describe "::store_dir" do
-    it "is the predetermined enco folder" do
-      stub_const("Audio::STORE_DIRS", { enco: "features" })
-      audio = build :enco_audio
-      Audio::EncoAudio.store_dir(audio).should eq "features"
+
+  describe "#sync" do
+    it "sets the mp3 and status if the file exists" do
+      # Set status and mp3 to nil so we can check that this method sets them
+      enco = create :enco_audio, :live_enco, status: nil, mp3: nil
+      enco.mp3.file.should eq nil
+      enco.status.should eq Audio::STATUS_WAIT
+
+      enco.sync
+      enco.mp3.file.should_not eq nil
+      enco.status.should eq Audio::STATUS_LIVE
+    end
+
+    it "doesn't do anything if the file doesn't exist" do
+      # This enco information doesn't exist
+      enco = create :enco_audio, enco_date: Date.new(2012, 10, 2), enco_number: "9999"
+      enco.mp3.file.should eq nil
+      enco.status.should eq Audio::STATUS_WAIT
+
+      enco.sync
+      enco.mp3.file.should eq nil
+      enco.status.should eq Audio::STATUS_WAIT
     end
   end
-  
+
+  describe '#filename' do
+    it "makes the filename based on enco number and date" do
+      audio = build :enco_audio, enco_number: "1234", enco_date: Date.new(1988, 10, 21)
+      audio.filename.should eq "19881021_features1234.mp3"
+    end
+  end
+
   #----------------
 
-  describe "::bulk_sync" do
-    it "sends to Audio::Sync::bulk_sync_awaiting_audio" do
-      Audio::Sync.should_receive(:bulk_sync_awaiting_audio).with(Audio::EncoAudio)
-      Audio::EncoAudio.bulk_sync
+  describe '#store_dir' do
+    it "is the predetermined enco folder" do
+      audio = build :enco_audio
+      audio.store_dir.should eq "features"
     end
   end
-  
-  #----------------
-  
-  describe "#sync" do
-    it "sends to Audio::Sync::sync_if_file_exists" do
-      enco = create :enco_audio
-      Audio::Sync.should_receive(:sync_if_file_exists).with(enco)
-      enco.sync
-    end
-  end
+
 
   describe "#path" do
     it "returns the store_dir and the filename" do
@@ -48,7 +70,6 @@ describe Audio::EncoAudio do
 
   describe "#full_path" do
     it "returns the server path to the mp3 if mp3 is present" do
-      Rails.application.config.scpr.stub(:media_root) { Rails.root.join("spec/fixtures/media") }
       audio = create :audio, :enco
       audio.full_path.should eq Rails.root.join("spec/fixtures/media/audio/#{audio.path}").to_s
 
@@ -59,26 +80,25 @@ describe Audio::EncoAudio do
 
   describe "#url" do
     it "returns the full URL to the mp3 if it's live" do
-      audio = create :audio, :enco, :live
+      audio = create :enco_audio, :live_enco
       audio.url.should eq "#{Audio::AUDIO_URL_ROOT}/#{audio.path}"
     end
 
     it "returns nil if not live" do
-      audio = create :audio, :enco
+      audio = create :enco_audio
       audio.url.should be_nil
     end
   end
 
   describe "#podcast_url" do
     it "returns the full podcast URL to the mp3 if it's live" do
-      audio = create :audio, :enco, :live
+      audio = create :enco_audio, :live_enco
       audio.podcast_url.should eq "#{Audio::PODCAST_URL_ROOT}/#{audio.path}"
     end
 
     it "returns nil if mp3 not live" do
-      audio = create :audio, :enco
+      audio = create :enco_audio, :enco
       audio.podcast_url.should be_nil
     end
   end
-
 end
