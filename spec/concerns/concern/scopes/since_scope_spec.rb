68a0620b243994a449e0f8fc9c98da04967fbd92
Bryan Ricker <bricker88@gmail.com>
2013-07-30 14:24:48 -0700

    Work around the dumb time bugs

--- spec/concerns/concern/scopes/since_scope_spec.rb
+++ spec/concerns/concern/scopes/since_scope_spec.rb
@@ -2,14 +2,17 @@ require "spec_helper"
 
 describe Concern::Scopes::SinceScope do
   it "gets only the records from the specified limit" do
+    # There is a bug in the tests somewhere that is leaving a bunch of
+    # these objects in the database, but I can't find it, so...
+    TestClass::Story.destroy_all
+
     t = Time.now
-    Time.stub(:now) { t }
 
-    recent     = create :test_class_story, published_at: 1.day.ago
-    really_old = create :test_class_story, published_at: 1.year.ago
+    recent     = create :test_class_story, published_at: t - 1.day
+    really_old = create :test_class_story, published_at: t - 1.year
 
-    TestClass::Story.since(1.hour.ago).should eq []
-    TestClass::Story.since(3.days.ago).should eq [recent]
-    TestClass::Story.since(2.years.ago).sort.should eq [recent, really_old].sort
+    TestClass::Story.since(t - 1.hour).should eq []
+    TestClass::Story.since(t - 3.days).should eq [recent]
+    TestClass::Story.since(t - 2.years).sort.should eq [recent, really_old].sort
   end
 end
