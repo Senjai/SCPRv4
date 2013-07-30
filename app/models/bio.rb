585a9f81e5a1a4e0f353b8545d9d8dc5b06213ae
Bryan Ricker <bricker88@gmail.com>
2013-07-30 12:14:55 -0700

    Cleanup

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -12,7 +12,7 @@ class Bio < ActiveRecord::Base
   #--------------
   # Scopes
   scope :visible, -> { where(is_public: true) }
-  
+
   #--------------
   # Associations
   belongs_to  :user,    class_name: "AdminUser"
@@ -23,7 +23,7 @@ class Bio < ActiveRecord::Base
   validates :slug, uniqueness: true
   validates :name, presence: true
   validates :last_name, presence: true
-  
+
   #--------------
   # Callbacks
   before_validation :set_last_name, if: -> { self.last_name.blank? }
@@ -34,13 +34,13 @@ class Bio < ActiveRecord::Base
   end
 
   #--------------
-  # Sphinx  
+  # Sphinx
   define_index do
     indexes name, sortable: true
     indexes title
     indexes email
   end
-    
+
   #----------
 
   class << self
@@ -50,48 +50,48 @@ class Bio < ActiveRecord::Base
       self.order("last_name").map { |bio| [bio.name, bio.id] }
     end
   end
-  
+
   #----------
-  
+
   def indexed_bylines(page=1, per_page=15)
     # Sphinx max_matches limits how much it can offset results, so for Bios with a lot
     # of pages of Bylines, we have to fallback to an actual SQL query if the offset is
     # too high. Run some Ruby methods on the byines to mimic SQL's order and conditions.
     if page.to_i > (SPHINX_MAX_MATCHES / per_page.to_i)
       bylines = self.bylines.includes(:content).all
-                    
+
       Kaminari.paginate_array(bylines.select { |b| b.content.published? }
              .sort_by { |b| b.content.published_at }
              .reverse)
              .page(page).per(per_page)
     else
-      ContentByline.search('', 
+      ContentByline.search('',
         :order        => :published_at,
         :sort_mode    => :desc,
         :per_page     => per_page,
         :page         => page,
-        :with         => { 
-          :user_id => self.id, 
+        :with         => {
+          :user_id => self.id,
           :status  => ContentBase::STATUS_LIVE
         }
       )
     end
   end
-  
+
   #---------------------
-  
+
   def headshot
     if self.asset_id?
       @headshot ||= AssetHost::Asset.find(self.asset_id)
     end
   end
-  
+
   #---------------------
-  
+
   def json
     { asset: self.headshot }
   end
-  
+
   #---------------------
 
   def route_hash
