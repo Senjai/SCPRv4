a4591b9224fd9065b55a34442352547e9e253d72
Bryan Ricker <bricker88@gmail.com>
2013-08-01 14:52:36 -0700

    Use production path for media, just to be safe

--- db/migrate/20130729193008_migrate_mp3_paths_to_mp3_urls.rb
+++ db/migrate/20130729193008_migrate_mp3_paths_to_mp3_urls.rb
@@ -2,7 +2,7 @@ class MigrateMp3PathsToMp3Urls < ActiveRecord::Migration
   def up
     Audio::DirectAudio.all.each do |audio|
       path = audio.external_url
-      audio.update_column :external_url, File.join(Audio::AUDIO_URL_ROOT, path.gsub(/\A(audio)/, ""))
+      audio.update_column :external_url, File.join("http://media.scpr.org/audio/", path.gsub(/\A(audio)/, ""))
       audio.update_column :mp3, nil
     end
   end
