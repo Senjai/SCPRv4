14c9b7cb47b5068ac4bc651a9e3fb34aca80b0cf
Bryan Ricker <bricker88@gmail.com>
2013-08-02 14:19:20 -0700

    Remove web1 from deploy temporarily

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -13,8 +13,8 @@ web2  = "66.226.4.227"
 web4  = "66.226.4.240"
 media = "66.226.4.228"
 
-role :app,      web1, web2, web4
-role :web,      web1, web2, web4
+role :app,      web2, web4
+role :web,      web2, web4
 role :workers,  media
 role :db,       web2, :primary => true
 role :sphinx,   media
