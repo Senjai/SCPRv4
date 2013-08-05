51fdfa5375bec7665d4baac5d03249660c997928
Bryan Ricker <bricker88@gmail.com>
2013-08-05 16:48:28 -0700

    Spacing cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
     "trujillo.png"
   ]
 
-  
+
   def present(object, klass=nil)
     klass ||= "#{object.class}Presenter".constantize
     presenter = klass.new(object, self)
@@ -22,12 +22,12 @@ module ApplicationHelper
 
 
   #---------------------------
-  # render_content takes a ContentBase object and a context, and renders 
+  # render_content takes a ContentBase object and a context, and renders
   # using the most specific version of that context it can find.
-  # 
-  # For instance, if your content is a "news/story" and your context is 
+  #
+  # For instance, if your content is a "news/story" and your context is
   # "lead", render_content will try:
-  # 
+  #
   # * shared/content/news/story/lead
   # * shared/content/news/lead
   # * shared/content/default/lead
@@ -50,8 +50,8 @@ module ApplicationHelper
       end
 
       html << render(
-        "shared/content/#{partial}", 
-        :article => article, 
+        "shared/content/#{partial}",
+        :article => article,
         :options => options
       )
     end
@@ -59,12 +59,12 @@ module ApplicationHelper
     html.html_safe
   end
 
-  #---------------------------  
-  # render_asset takes a ContentBase object and a context, and renders using 
-  # an optional context_asset_scheme attribute on the object.  
+  #---------------------------
+  # render_asset takes a ContentBase object and a context, and renders using
+  # an optional context_asset_scheme attribute on the object.
   #
-  # For example, given a context of "story", render_asset will check for a 
-  # story_asset_scheme attribute on the object.  If found (let's assume with a 
+  # For example, given a context of "story", render_asset will check for a
+  # story_asset_scheme attribute on the object.  If found (let's assume with a
   # value of "wide"), it will try to render:
   #
   # * shared/assets/story/wide
@@ -77,7 +77,7 @@ module ApplicationHelper
     if article.assets.empty?
       return fallback ? render("shared/assets/#{context}/fallback", article: article) : ''
     end
-    
+
     # look for a scheme on the content object
     attribute = "#{context}_asset_scheme"
     scheme = content.respond_to?(attribute) ? content.send(attribute) : "default"
@@ -89,55 +89,55 @@ module ApplicationHelper
       "#{context}/default",
       "default/default"
     ]
-    
-    partial = tmplt_opts.find do |template| 
+
+    partial = tmplt_opts.find do |template|
       self.lookup_context.exists?(template, ["shared/assets"], true)
     end
 
     render "shared/assets/#{partial}", assets: article.assets, article: article
   end
-  
+
   #----------
-  
+
   def random_headshot
     image_tag "personalities/#{HEADSHOTS.sample}"
   end
-  
+
   #----------
-  
+
   def smart_date_js(datetime, options={})
     return '' if !datetime.respond_to?(:strftime)
 
     time_tag datetime, '', {
-      "class"         => "#{options[:class]} smart smarttime", 
+      "class"         => "#{options[:class]} smart smarttime",
       "data-unixtime" => datetime.to_i
     }.merge(options.except(:class))
   end
-  
+
   #----------
   # Render a byline for the passed-in content
   # If links is set to false, and the content has
   # bylines, this will yield the same as +content.byline+
-  # 
+  #
   # If the content doesn't have bylines, just return
   # "KPCC" for opengraph stuff.
   def render_byline(content, links=true)
     return "KPCC" if !content.respond_to?(:joined_bylines)
-    
+
     elements = content.joined_bylines do |bylines|
       link_bylines(bylines, links)
     end
 
     ContentByline.digest(elements).html_safe
   end
-  
+
   #---------------------------
-  
+
   def render_contributing_byline(content,links=true)
     elements = content.joined_bylines do |bylines|
       link_bylines(bylines, links)
     end
-    
+
     if elements[:contributing].present?
       "With contributions by #{elements[:contributing]}".html_safe
     else
@@ -161,14 +161,14 @@ module ApplicationHelper
       end
     end
   end
-  
+
   #---------------------------
-  # Convert a given number of seconds into a human-readable duration. 
+  # Convert a given number of seconds into a human-readable duration.
   def format_duration(secs)
     if !secs
       return ''
     end
-    
+
     [[60, :sec], [60, :min], [24, :hr], [1000, :days]].map{ |count, name|
       if secs > 0
         secs, n = secs.divmod(count)
@@ -176,9 +176,9 @@ module ApplicationHelper
       end
     }.compact.reverse.join(' ')
   end
-  
+
   #----------
-  
+
   def latest_arts(limit=12)
     ContentBase.search({
       :classes     => [NewsStory, BlogEntry, ShowSegment, ContentShell],
@@ -187,9 +187,9 @@ module ApplicationHelper
       :without     => { category: '' }
     }).map(&:to_article)
   end
-  
+
   #----------
-  
+
   def latest_news(limit=12)
     ContentBase.search({
       :classes     => [NewsStory, BlogEntry, ShowSegment, ContentShell],
@@ -197,7 +197,7 @@ module ApplicationHelper
       :with        => { category_is_news: true }
     }).map(&:to_article)
   end
-  
+
   #----------
 
   def link_to_audio(title, article, options={}) # This needs to be more useful
@@ -209,7 +209,7 @@ module ApplicationHelper
     options["data-duration"] = object.audio.first.duration
     content_tag :div, link_to(title, object.audio.first.url, options), class: "story-audio inline"
   end
-  
+
   #---------------------------
 
   def twitter_profile_url(handle)
@@ -224,13 +224,13 @@ module ApplicationHelper
   end
 
   #---------------------------
-  
+
   def relaxed_sanitize(html)
     Sanitize.clean(html.to_s.html_safe, Sanitize::Config::RELAXED)
   end
-  
+
   #---------------------------
-  
+
   def split_collection(array, num)
     last_num  = array.size - num
     first     = array.first(num)
@@ -239,7 +239,7 @@ module ApplicationHelper
   end
 
   #----------
-  
+
   def pij_source(content, options={})
     message = options[:message] || "This story was informed by KPCC listeners."
 
@@ -252,7 +252,7 @@ module ApplicationHelper
   # Render a timestamp inside of a time tag.
   #
   # time_tag uses i18n's `localize` method, which raises
-  # if the date passed in doesn't respond to strftime, so we 
+  # if the date passed in doesn't respond to strftime, so we
   # need to check that this is the case before rendering the
   # time tag. Otherwise previewing unpublished content breaks.
   def timestamp(datetime)
@@ -261,7 +261,7 @@ module ApplicationHelper
     end
   end
 
-  
+
   #----------
 
   def comment_widget_for(object, options={})
@@ -288,16 +288,16 @@ module ApplicationHelper
     object.respond_to?(:disqus_identifier)
   end
 
-  
+
   #----------
-  
+
   def content_widget(partial, object, options={})
     partial = partial.chars.first == "/" ? partial : "shared/cwidgets/#{partial}"
     render(partial, { article: object.to_article, cssClass: "" }.merge(options))
   end
-  
+
   alias_method :widget, :content_widget
-  
+
   #---------------
   # These two methods are taken from EscapeUtils
   def html_escape(string)
