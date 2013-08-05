6522a6e0e0151b15ccc5afa2f860f7da218b02fc
Bryan Ricker <bricker88@gmail.com>
2013-08-05 12:07:30 -0700

    Add specs for abstract model methods

--- spec/concerns/concern/methods/abstract_model_methods_spec.rb
+++ spec/concerns/concern/methods/abstract_model_methods_spec.rb
@@ -1,4 +1,56 @@
 require 'spec_helper'
 
-describe AbstractModelMethods do
+describe Concern::Methods::AbstractModelMethods do
+  describe '#==' do
+    it 'is true if they are the same class and id is the same' do
+      obj1 = AbstractModel.new(id: 1)
+      obj2 = AbstractModel.new(id: 1)
+      (obj1 == obj2).should eq true
+    end
+
+    it 'is false if the class is different' do
+      obj1 = AbstractModel.new(id: 1)
+      obj2 = Object.new
+
+      (obj1 == obj2).should eq false
+    end
+
+    it 'is false if the ids are different' do
+      obj1 = AbstractModel.new(id: 1)
+      obj2 = AbstractModel.new(id: 2)
+      (obj1 == obj2).should eq false
+    end
+  end
+
+  describe '#cache_key' do
+    it 'is the original object cache_key' do
+      klass = Struct.new(:cache_key, :updated_at)
+      person = klass.new("person:123", Time.now)
+      obj = AbstractModel.new(original_object: person)
+      obj.cache_key.should eq person.cache_key
+    end
+
+    it 'is nil if the original object does no have a cache key' do
+      klass = Struct.new(:updated_at)
+      person = klass.new(Time.now)
+      obj = AbstractModel.new(original_object: person)
+      obj.cache_key.should eq nil
+    end
+  end
+
+  describe '#updated_at' do
+    it 'is the original object updated_at' do
+      klass = Struct.new(:cache_key, :updated_at)
+      person = klass.new("person:123", Time.now)
+      obj = AbstractModel.new(original_object: person)
+      obj.updated_at.should eq person.updated_at
+    end
+
+    it 'is nil if the original object does no have a cache key' do
+      klass = Struct.new(:cache_key)
+      person = klass.new(Time.now)
+      obj = AbstractModel.new(original_object: person)
+      obj.updated_at.should eq nil
+    end
+  end
 end
