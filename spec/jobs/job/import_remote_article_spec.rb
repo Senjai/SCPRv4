af2aca894ed9a99b430be440ce0bdf07caf2c0de
Bryan Ricker <bricker88@gmail.com>
2013-08-07 19:16:02 -0700

    Fill in some pending specs

--- spec/jobs/job/import_remote_article_spec.rb
+++ spec/jobs/job/import_remote_article_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Job::ImportRemoteArticle do
-  pending
+    before :each do
+      FakeWeb.register_uri(:get, %r|api\.npr|, 
+        :content_type => "application/json",
+        :body => load_fixture('api/npr/story.json')
+      )
+    end
+
+  it "finds the article and imports it" do
+    # This headline comes from the fixture
+    # Setting the headline here is just for demonstration
+    article = create :npr_article, headline: "Four Men In A Small Boat Face The Northwest Passage"
+    Job::ImportRemoteArticle.perform(article.id, "NewsStory")
+
+    story = NewsStory.all.first
+    story.headline.should eq "Four Men In A Small Boat Face The Northwest Passage"
+  end
+
+  it "raises an error if the story isn't found" do
+    article = create :npr_article, headline: "Four Men In A Small Boat Face The Northwest Passage"
+    RemoteArticle.any_instance.stub(:import)
+
+    expect { Job::ImportRemoteArticle.perform(article.id, "NewsStory") }.to raise_error
+  end
 end
