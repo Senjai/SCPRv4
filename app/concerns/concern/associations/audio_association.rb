9560c66b9c1d054bb9ff45207a911280f4071398
Bryan Ricker <bricker88@gmail.com>
2013-08-07 17:31:26 -0700

    Cleanup

--- app/concerns/concern/associations/audio_association.rb
+++ app/concerns/concern/associations/audio_association.rb
@@ -7,15 +7,15 @@ module Concern
   module Associations
     module AudioAssociation
       extend ActiveSupport::Concern
-      
+
       included do
         has_many :audio, as: :content, order: "position", dependent: :destroy
         accepts_nested_attributes_for :audio, allow_destroy: true, reject_if: :should_reject_audio?
       end
-      
+
+
       private
-      
-      #------------------
+
       # If all of these attributes are blank, reject it,
       # because obviously they weren't trying to attach
       # any audio.
@@ -26,9 +26,9 @@ module Concern
       # Can't use `all_blank?` because `position` has a
       # default value (0).
       def should_reject_audio?(attributes)
-        attributes['mp3'].blank? && 
-        attributes['enco_number'].blank? && 
-        attributes['enco_date'].blank? && 
+        attributes['mp3'].blank? &&
+        attributes['enco_number'].blank? &&
+        attributes['enco_date'].blank? &&
         attributes['external_url'].blank? &&
         attributes['description'].blank? &&
         attributes['byline'].blank?
