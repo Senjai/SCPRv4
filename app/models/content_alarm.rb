d6a5fa97f77672455e33d4697063794ff541686c
Bryan Ricker <bricker88@gmail.com>
2013-08-12 09:38:08 -0700

    Use content.publish method in ContentAlarm

--- app/models/content_alarm.rb
+++ app/models/content_alarm.rb
@@ -25,20 +25,23 @@ class ContentAlarm < ActiveRecord::Base
 
 
   # Fire an alarm.
-  # TODO: Instead of updating attribute, just define a `publish` instance method,
-  # so the class can decide how to publish instead of this class.
   def fire
-    if self.can_fire?
-      ContentAlarm.log "Firing ContentAlarm ##{self.id} for #{self.content.simple_title}"
-      if self.content.update_attributes(status: ContentBase::STATUS_LIVE)
-        ContentAlarm.log "Published #{self.content.simple_title}. Removing this alarm."
+    if can_fire?
+      log "Firing ContentAlarm ##{self.id} for " \
+          "#{self.content.simple_title}"
+
+      if self.content.publish
+        log "Published #{self.content.simple_title}. " \
+            "Removing this alarm."
         self.destroy
       else
-        ContentAlarm.log "Couldn't save #{self.content.simple_title}. Will be tried again."
+        log "Couldn't save #{self.content.simple_title}. " \
+            "Will be tried again next time."
         false
       end
     else
-      ContentAlarm.log "Can't fire ContentAlarm ##{self.id} for #{self.content.simple_title}"
+      log "Can't fire ContentAlarm ##{self.id} " \
+          "for #{self.content.simple_title}."
       false
     end
   end
@@ -50,17 +53,13 @@ class ContentAlarm < ActiveRecord::Base
     self.fire_at <= Time.now
   end
 
-  #---------------------
-  # Can fire if this alarm is pending, and if the content is 
-  # Pending -OR- Published... in the case that it's published, 
+
+  private
+
+  # Can fire if this alarm is pending, and if the content is
+  # Pending -OR- Published... in the case that it's published,
   # it will just serve to "touch" the content.
-  #
-  # Note that SCPRv4 destroys content alarms when content moves 
-  # from Pending -> Not Pending, so once mercer is gone, there 
-  # shouldn't be any alarms with content that ISN'T Pending,
-  # so the extra `content.published?` condition can probably
-  # go away at that point.
   def can_fire?
-    self.pending? && (self.content.pending? || self.content.published?)
+    self.pending? && self.content.pending?
   end
 end
