07b38a86f47e75c15f53b92e7d9553b65ada06ef
Bryan Ricker <bricker88@gmail.com>
2013-08-13 12:22:25 -0700

    csv script changes

--- util/scripts/csv_generation/entries_by_blog.rb
+++ util/scripts/csv_generation/entries_by_blog.rb
@@ -1,15 +1,17 @@
+# Rails.root.join("log", "#{filename}-#{Time.now.strftime("%F")}.csv")
+
 require "csv"
  
 filename = "represent-export"
 
 blogs = [
-  "politics"
+  "southla"
 ]
 
 Rails.application.default_url_options[:host] = "http://scpr.org"
 
-low  = Time.new(2013, 4, 14, 0, 0, 0) - 1
-high = Time.new(2013, 6, 30, 0, 0, 0) - 1
+low  = Time.new(2012, 8, 1, 0, 0, 0) - 1
+high = Time.new(2013, 7, 31, 0, 0, 0) - 1
 
 #------------------
 
@@ -19,11 +21,11 @@ blogs.each do |blog|
   blog = Blog.find_by_slug(blog)
   
   blog.entries.where("published_at > :low and published_at <= :high", low: low, high: high).published.reorder("published_at").each do |entry|
-    rows.push [entry.published_at, entry.to_title, entry.byline, entry.public_url]
+    rows.push [entry.published_at, entry.to_title, entry.teaser, entry.byline]
   end
 end
- 
-CSV.open(Rails.root.join("log", "#{filename}-#{Time.now.strftime("%F")}.csv"), "w+") do |csv|
+
+CSV.open("/Users/bryan/projects/Data Backups/hyper-2013-08-13.csv", "a+") do |csv|
   rows.each do |row|
     csv << row
   end
