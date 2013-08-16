13a09ea1d4b9f24911cf4554385bbd88278a871b
Bryan Ricker <bricker88@gmail.com>
2013-08-16 16:32:24 -0700

    Add promises to sphinx indexing callbacks

--- app/concerns/concern/callbacks/sphinx_index_callback.rb
+++ app/concerns/concern/callbacks/sphinx_index_callback.rb
@@ -9,15 +9,31 @@ module Concern
       extend ActiveSupport::Concern
 
       included do
-        after_save :enqueue_sphinx_index_for_class, if: -> { self.changed? }
-        after_destroy :enqueue_sphinx_index_for_class
+        after_save :promise_class_index,
+          :if => -> { self.changed? }
+
+        after_destroy :promise_class_index
+        after_commit :enqueue_sphinx_index_for_class
       end
+
 
       private
 
+      def promise_class_index
+        @_will_index_class = true
+      end
+
+      def reset_index_promises
+        @_will_index_class = nil
+      end
+
       # Enqueue a sphinx index for this model
       def enqueue_sphinx_index_for_class
-        Indexer.enqueue(self.class.name)
+        if @_will_index_class
+          Indexer.enqueue(self.class.name)
+        end
+
+        reset_index_promises
       end
     end
   end
