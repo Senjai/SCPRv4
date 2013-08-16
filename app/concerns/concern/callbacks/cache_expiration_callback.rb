bfa99aeb88f8e6b4c06644ead46bd787f61d7133
Bryan Ricker <bricker88@gmail.com>
2013-08-16 14:40:48 -0700

    Use promises for CacheExpirationCallback to know when to expire cache, but not do it until after save

--- app/concerns/concern/callbacks/cache_expiration_callback.rb
+++ app/concerns/concern/callbacks/cache_expiration_callback.rb
@@ -4,6 +4,14 @@
 # Expires cache
 # Requires the methods defined in PublishingMethods
 #
+# We have to set the "promises" before save so that we still have
+# access to the object's dirty attributes (status). Otherwise we
+# don't know if the article was just published, unpublished, or what.
+#
+# But we don't want to expire the cache before the record is actually
+# committed to the database, because there is a small chance that the
+# cache could be rewritten with the old object, before it's committed.
+#
 module Concern
   module Callbacks
     module CacheExpirationCallback
@@ -11,25 +19,47 @@ module Concern
 
       included do
         include Concern::Methods::PublishingMethods
+        after_save :check_if_should_expire_cache
         after_commit :expire_cache
       end
 
       private
 
-      def expire_cache
+      def check_if_should_expire_cache
+        reset_cache_promises
+
         # If we are going from "published" -> "published" (still),
         # or we are going from "published" -> "unpublished",
         # just expire this object
         if (self.published? && !self.publishing?) || self.unpublishing?
-          Rails.cache.expire_obj(self)
+          @_will_expire_dependencies_on_self = true
         end
 
         # If we are going from "not published" -> "published".
         # Expire :new keys for the object's class and contentbase
         if self.publishing?
+          @_will_expire_dependencies_on_new_objects = true
+        end
+      end
+
+
+      def expire_cache
+        if @_will_expire_dependencies_on_self
+          Rails.cache.expire_obj(self)
+        end
+
+        if @_will_expire_dependencies_on_new_objects
           Rails.cache.expire_obj([self.class.content_key, "new"].join(":"))
           Rails.cache.expire_obj("contentbase:new")
         end
+
+        reset_cache_promises
+      end
+
+
+      def reset_cache_promises
+        @_will_expire_dependencies_on_self = nil
+        @_will_expire_dependencies_on_new_objects = nil
       end
     end # CacheExpiration
   end # Callbacks
--- spec/concerns/concern/callbacks/cache_expiration_callback_spec.rb
+++ spec/concerns/concern/callbacks/cache_expiration_callback_spec.rb
@@ -3,20 +3,50 @@ require "spec_helper"
 describe Concern::Callbacks::CacheExpirationCallback do
   it "expires the object if it's already published" do
     story = create :test_class_story, :published
-    Rails.cache.should_receive(:expire_obj).with(story)
-    story.update_attributes(status: ContentBase::STATUS_LIVE, headline: "wat")
+
+    set           = "obj:#{story.obj_key}"
+    fragment_key  = "coolcachebro"
+
+    $redis.set(fragment_key, "harry potter")
+    $redis.sadd(set, fragment_key)
+
+    $redis.get(fragment_key).should eq "harry potter"
+    story.update_attributes(status: ContentBase::STATUS_LIVE)
+    $redis.get(fragment_key).should eq nil
   end
 
   it "expires the object if it's unpublishing" do
     story = create :test_class_story, :published
-    Rails.cache.should_receive(:expire_obj).with(story)
+
+    set           = "obj:#{story.obj_key}"
+    fragment_key  = "coolcachebro"
+
+    $redis.set(fragment_key, "harry potter")
+    $redis.sadd(set, fragment_key)
+
+    $redis.get(fragment_key).should eq "harry potter"
     story.update_attributes(status: ContentBase::STATUS_PENDING)
+    $redis.get(fragment_key).should eq nil
   end
 
   it "expires new keys if it's publishing" do
     story = create :test_class_story, :pending
-    Rails.cache.should_receive(:expire_obj).with(story.class.content_key + ":new")
-    Rails.cache.should_receive(:expire_obj).with("contentbase:new")
+
+    set1            = "obj:test/class/stories:new"
+    set2            = "obj:contentbase:new"
+    fragment_key1   = "bagel"
+    fragment_key2   = "burrito"
+
+    $redis.set(fragment_key1, "delicious")
+    $redis.set(fragment_key2, "extra-delicious")
+
+    $redis.sadd(set1, fragment_key1)
+    $redis.sadd(set2, fragment_key2)
+
+    $redis.get(fragment_key1).should eq "delicious"
+    $redis.get(fragment_key2).should eq "extra-delicious"
     story.update_attributes(status: ContentBase::STATUS_LIVE)
+    $redis.get(fragment_key1).should eq nil
+    $redis.get(fragment_key2).should eq nil
   end
 end
