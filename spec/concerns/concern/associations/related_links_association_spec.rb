944a42356a80ba924cb4efd414d927cae864bb40
Bryan Ricker <bricker88@gmail.com>
2013-08-30 09:42:51 -0700

    Work on some specs

--- spec/concerns/concern/associations/related_links_association_spec.rb
+++ spec/concerns/concern/associations/related_links_association_spec.rb
@@ -8,4 +8,19 @@ describe Concern::Associations::RelatedLinksAssociation do
       story.get_link("rss").should eq "rss.com/airtalk"
     end
   end
+
+  describe 'versioning' do
+    it 'creates a version when saving the parent object', focus: true do
+      story = create :test_class_story
+      link1 = build :related_link, content: nil
+      link2 = build :related_link, content: nil
+      story.related_links = [link1, link2] # calls save on story... :(
+
+      versions = story.versions
+      versions.size.should eq 2
+
+      versions.last.object_changes["related_links"][0].should be_empty
+      versions.last.object_changes["related_links"][1].should be_present
+    end
+  end
 end
