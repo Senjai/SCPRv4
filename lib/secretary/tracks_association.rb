c915fb337adcf3a55dca9916dda05aab77020f05
Bryan Ricker <bricker88@gmail.com>
2013-09-06 17:15:07 -0700

    More work on association versioning

--- lib/secretary/tracks_association.rb
+++ lib/secretary/tracks_association.rb
@@ -23,6 +23,7 @@ module Secretary
 
       associations.each do |name|
         module_eval <<-EOE, __FILE__, __LINE__ + 1
+          attr_writer :#{name}_were
           def #{name}_were
             @#{name}_were ||= association_was("#{name}")
           end
@@ -40,9 +41,18 @@ module Secretary
           def mark_#{name}_as_changed(object)
             mark_association_as_changed("#{name}", object)
           end
+
+
+          def clear_dirty_#{name}
+            clear_dirty_association(name)
+          end
         EOE
 
         before_save :"build_custom_changes_for_#{name}"
+        after_commit :"clear_dirty_#{name}"
+
+        send("before_add_for_#{name}=", Array(:"#{name}_were"))
+        send("before_remove_for_#{name}=", Array(:"#{name}_were"))
         send("after_add_for_#{name}=", Array(:"mark_#{name}_as_changed"))
         send("after_remove_for_#{name}=", Array(:"mark_#{name}_as_changed"))
       end
@@ -61,6 +71,10 @@ module Secretary
         send("should_reject_#{name}?", object.attributes.stringify_keys)
 
         self.custom_changes[name] = [send("#{name}_were"), send(name)]
+      end
+
+      def clear_dirty_association(name)
+        self.send("#{name}_were=", nil)
       end
     end
   end
