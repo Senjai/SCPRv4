2456e735ccc00cf1e263dd1d17cd05954fbc3bf6
Bryan Ricker <bricker88@gmail.com>
2013-09-13 15:37:03 -0700

    Finish this abomination of a CKEditor plugin

--- vendor/assets/javascripts/ckeditor/plugins/outpost-embeds/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/outpost-embeds/plugin.js
@@ -12,52 +12,66 @@
       icons: 'mediaembed',
       init: function(editor) {
         var self = this;
+
+        CKEDITOR.on('dialogDefinition', function(ev) {
+          var dialogName = ev.data.name;
+          var dialogDefinition = ev.data.definition;
+
+          if (dialogName == 'OutpostEmbedsDialog') {
+            var main = dialogDefinition.getContents('main');
+            var embeds = [];
+
+            $('#embeds-fields tr').each(function() {
+              var title = $(this).find('input[type=text]').val(),
+                  url   = $(this).find('input[type=url]').val();
+
+              if(url != "") {
+                embeds.push({
+                  title : title,
+                  url   : url
+                })
+              }
+            });
+
+            var items = [];
+            for(i=0; i < embeds.length; i++) {
+              var embed = embeds[i],
+                  title = embed.title,
+                  url   = embed.url
+
+              items.push([
+                title + " (" + url + ")",
+                "<a href='"+url+"' class='embed-placeholder'>"+title+"</a>"
+              ])
+            }
+
+            main.add({
+              id    : 'embed-selection',
+              type  : 'radio',
+              label : "Select Embed",
+              items : items
+            });
+          }
+        });
+
         CKEDITOR.dialog.add('OutpostEmbedsDialog', function (instance) {
+          this.embeds = [];
+
           return {
             title : 'Embeds',
             minWidth : 550,
             minHeight : 200,
-            contents : [
-              {
-                id : 'outpost-embeds',
-                expand : true,
-                elements : [
-                  {
-                    id            : 'embedArea',
-                    type          : 'textarea',
-                    label         : 'URL',
-                    autofocus     : 'autofocus',
-                  }
-                ]
-              }
-            ],
 
-            onLoad: function() {
-              this.embeds = [];
-              self = this;
-
-              $('#embeds-fields tr').each(function() {
-                var title = $(this).find('input[type=text]').val()
-                    url   = $(this).find('input[type=url]').val();
-
-                if(url != "") {
-                  self.embeds.push({
-                    title : title,
-                    url   : url
-                  });
-                }
-              });
-            },
+            contents: [{
+              id: "main",
+              elements: []
+            }],
 
             onOk: function() {
-              for (var i = 0; i < window.frames.length; i++) {
-                if (window.frames[i].name == 'iframeMediaEmbed') {
-                  var content = window.frames[i].document.getElementById("embed").value;
-                }
-              }
-              div = instance.document.createElement('div');
-              div.setHtml(this.getContentElement('iframe', 'embedArea').getValue());
-              instance.insertElement(div);
+              var p = instance.document.createElement('p');
+              console.log(this.getValueOf('main', 'embed-selection'));
+              p.setHtml(this.getValueOf('main', 'embed-selection'));
+              instance.insertElement(p);
             }
           }; // return
         });
